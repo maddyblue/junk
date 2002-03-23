6738ffadbc8a0dced6ff1f27bbff8897f6e9a02b
trythil <trythil@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-23 18:01:23 +0000

    Added ReadTable method.  It works, for now.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@9 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/utility/DatabaseAccess.inc.php
+++ ci4/utility/DatabaseAccess.inc.php
@@ -1,9 +1,18 @@
 <?php
 
-/*	Database-access functions.	*/
+/*	Database access. */
 
 class DatabaseAccess
 {
+
+	/*	Connect: returns a handle to a database connection 
+	
+			Expected parameters:
+				SQLServer - server to connect to.
+				SQLUser - user to connect as.
+				SQLPassword - user's password.
+	*/
+
 	function Connect($parameters)
 	{
 		return mysql_connect(
@@ -13,9 +22,49 @@ class DatabaseAccess
 		);
 	}
 
+	/*	Disconnect: Closes a database connection
+			
+			Expected parameters:
+				handle - database connection to close.
+	*/
+
+	function Disconnect($handle)
+	{
+		mysql_close($handle);
+	}
+
+	/*	ReadTable: Given a query, execute that query, and retrieve
+		all data in row/column format.  We use a hash table of arrays, 
+		i.e.
+
+		$hash["Column 1"] = ["A", "B", "C", "D"]
+		$hash["Column 2"] = ["1", "2", "3", "4"]
+
+		Expected parameters:
+			Handle - database connection to use.
+			Query - query to execute.
+			Database - database to perform this query on.
+	*/
+
 	function ReadTable($parameters)
 	{
-		
+		$ret = "";
+		$counter = 0;
+		$dbq = mysql_db_query(
+			$parameters{"Database"},
+			$parameters{"Query"},
+			$parameters{"Handle"}
+		);
+
+		while($row = mysql_fetch_assoc($dbq)) {
+			for($i = 1; $i <= sizeof($row); $i++) {
+				$ret{key($row)}[$counter] = $row{key($row)};
+				next($row);
+			}
+			$counter++;
+		}
+
+		return $ret;
 	}
 };
 	
