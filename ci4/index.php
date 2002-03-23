b19c95b03e832c9da71f077a65bd9edae7f5b1b8
trythil <trythil@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-23 18:03:49 +0000

    Added demo of DatabaseAccess object.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@10 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/index.php
+++ ci4/index.php
@@ -8,4 +8,57 @@ $act = "print $i";
 
 $c->PrepareAction("1", $act);
 $c->DoAction("1");
+
+print "<br>\n";
+
+/* Demonstrating DatabaseAccess object. */
+
+include_once("utility/DatabaseAccess.inc.php");
+$db = new DatabaseAccess;
+
+$con["SQLServer"] = "localhost";
+$con["SQLUser"] = "trythbot";
+$con["SQLPassword"] = "NULLIFIED";
+
+$params["Handle"] = $db->Connect($con);
+$params["Query"] = "select * from commands";
+$params["Database"] = "trythbot";
+
+$data = $db->ReadTable($params);
+
+?>
+
+<table border=1>
+<tr>
+<?
+
+for($i = 1; $i <= sizeof($data); $i++)
+{
+	$k = key($data);
+	print "<td>$k</td>";
+	next($data);
+}
+
+?>
+
+</tr>
+<tr>
+<?
+
+reset($data);
+
+for($i = 1; $i <= sizeof($data); $i++)
+{
+	print "<td valign=top>";
+	print "<table border=1>";
+	print "<tr valign=top>";
+	$k = key($data);
+	for($j = 1; $j <= sizeof($data{$k}); $j++)
+	{
+		print "<td>" . $data{$k}[$j] . "</td></tr>";
+	}
+	print "</table></td>";
+	next($data);
+}
+$db->Disconnect($params["Handle"]);
 ?>
