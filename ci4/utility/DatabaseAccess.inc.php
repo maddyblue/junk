cdb4210ef98278db8f6016542b66b41ce862f6e9
trythil <trythil@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-23 08:33:26 +0000

    A couple of basic functions -- fleshing out the class.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@5 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/utility/DatabaseAccess.inc.php
+++ ci4/utility/DatabaseAccess.inc.php
@@ -2,4 +2,21 @@
 
 /*	Database-access functions.	*/
 
+class DatabaseAccess
+{
+	function Connect($parameters)
+	{
+		return mysql_connect(
+			$parameters{"SQLServer"},
+			$parameters{"SQLUser"},
+			$parameters{"SQLPassword"}
+		);
+	}
+
+	function ReadTable($parameters)
+	{
+		
+	}
+};
+	
 ?>
