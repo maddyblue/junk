55a280b509e0982129eee69d59d05ab59879d1fa
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-24 23:13:55 +0000

    Added vars for the individual databases (main, item, auction)
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@18 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Database.inc.php
+++ ci4/config/Database.inc.php
@@ -2,7 +2,30 @@
 
 /*	MySQL database connection configuration.	*/
 
-$CIConfig{"SQLPassword"} = "nil";
-$CIConfig{"SQLHost"} = "localhost";
+// Globals
+$CIConfig{"SQLUser"}       = "usersql";
+$CIConfig{"SQLPassword"}   = "user";
+$CIConfig{"SQLHost"}       = "localhost";
+
+// Main database
+$CIConfig1{"SQLPort"}    = "3306";
+$CIConfig1{"SQLSocket"}  = "/tmp/mysql.sock";
+
+// Item database
+$CIConfig2{"SQLPort"}    = "33060";
+$CIConfig2{"SQLSocket"}  = "/tmp/mysql.ciitems.sock";
+
+// Auctions
+$CIConfig3{"SQLPort"}    = "33062";
+$CIConfig3{"SQLSocket"}  = "/tmp/mysql.ciauction.sock";
+
+/* Proposed changes:
+
+The items port to 33061, and its socket to /tmp/mysql.ciitem.sock.  This will
+reflect the non-plural standard used by the database, and make the port numbers
+not jump up two, with an empty on inbetween.
+-dolmant
+
+*/
 
 ?>
