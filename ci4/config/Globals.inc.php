23996df1a033ed43e747356f2728a94446bb25ca
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-24 23:12:48 +0000

    Added CI_DOMAIN and CI_PATH vars
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@17 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -2,6 +2,8 @@
 
 /*	CI globals.	*/
 
-$CI_HOME = "/var/www/ci4";
+$CI_HOME      = "/var/www/ci4";
+$CI_DOMAIN    = "crescentisland.com";
+$CI_PATH      = ""; // on the old site (ci3) this would have been "/ci"
 
 ?>
