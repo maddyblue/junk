7cc081a8ea5ce67a006384adeea1523bf09d7305
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-24 10:07:16 +0000

    Long style php tag added
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@15 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/utility/URLUtil.inc.php
+++ ci4/utility/URLUtil.inc.php
@@ -1,16 +1,16 @@
-<?
+<?php
 
 	/*
 		URLUtil
 		khraythia	[twisted_cicatrix@yahoo.com]
-		
+
 		A PHP object designed to aid in the parsing of URLs,
 		esp. query strings.
 
 		Last modification: 23 Mar 2001 1122 MST
 			- adapted to CI 4 class structure
 	*/
-	
+
 	/*
 			- ParseQString($str)
 				Takes a query string and parses data into key/value pairs.
