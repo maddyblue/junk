d64e844866d63157f94c5aa3a23495771d9d9b5d
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-03-25 07:54:52 +0000

    Changed to make sense with mysql_connect
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@23 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Database.inc.php
+++ ci4/config/Database.inc.php
@@ -2,28 +2,27 @@
 
 /*	MySQL database connection configuration.	*/
 
-// Globals
+// Main database
 $CIConfig{"SQLUser"}       = "usersql";
 $CIConfig{"SQLPassword"}   = "user";
 $CIConfig{"SQLHost"}       = "localhost";
 
-// Main database
-$CIConfig1{"SQLPort"}    = "3306";
-$CIConfig1{"SQLSocket"}  = "/tmp/mysql.sock";
-
 // Item database
-$CIConfig2{"SQLPort"}    = "33060";
-$CIConfig2{"SQLSocket"}  = "/tmp/mysql.ciitems.sock";
+$CIConfig2{"SQLUser"}       = "usersql";
+$CIConfig2{"SQLPassword"}   = "user";
+$CIConfig2{"SQLHost"}       = "localhost:33060";
 
 // Auctions
-$CIConfig3{"SQLPort"}    = "33062";
-$CIConfig3{"SQLSocket"}  = "/tmp/mysql.ciauction.sock";
+$CIConfig3{"SQLUser"}       = "usersql";
+$CIConfig3{"SQLPassword"}   = "user";
+$CIConfig3{"SQLHost"}       = "localhost:33062";
 
 /* Proposed changes:
 
 The items port to 33061, and its socket to /tmp/mysql.ciitem.sock.  This will
 reflect the non-plural standard used by the database, and make the port numbers
-not jump up two, with an empty on inbetween.
+not jump up two, with an empty on inbetween.  Also, CIConfig to CIConfig1 (or,
+similarly, CIConfig[23] to CIConfig[12])?
 -dolmant
 
 */
