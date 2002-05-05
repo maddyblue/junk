f7cc638088d647a7c0b309079e80950580080e69
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-05 22:41:05 +0000

    Standardized variable names and ports
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@35 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Database.inc.php
+++ ci4/config/Database.inc.php
@@ -3,28 +3,18 @@
 /*	MySQL database connection configuration.	*/
 
 // Main database
-$CIConfig{"SQLUser"}       = "usersql";
-$CIConfig{"SQLPassword"}   = "user";
-$CIConfig{"SQLHost"}       = "localhost";
+$CIConfig1{"SQLUser"}       = "usersql";
+$CIConfig1{"SQLPassword"}   = "user";
+$CIConfig1{"SQLHost"}       = "localhost";
 
 // Item database
 $CIConfig2{"SQLUser"}       = "usersql";
 $CIConfig2{"SQLPassword"}   = "user";
-$CIConfig2{"SQLHost"}       = "localhost:33060";
+$CIConfig2{"SQLHost"}       = "localhost:33062";
 
 // Auctions
 $CIConfig3{"SQLUser"}       = "usersql";
 $CIConfig3{"SQLPassword"}   = "user";
-$CIConfig3{"SQLHost"}       = "localhost:33062";
-
-/* Proposed changes:
-
-The items port to 33061, and its socket to /tmp/mysql.ciitem.sock.  This will
-reflect the non-plural standard used by the database, and make the port numbers
-not jump up two, with an empty on inbetween.  Also, CIConfig to CIConfig1 (or,
-similarly, CIConfig[23] to CIConfig[12])?
--dolmant
-
-*/
+$CIConfig3{"SQLHost"}       = "localhost:33063";
 
 ?>
