eb16e3fcb196cb063e24a14a4e9e6d8f32e8393c
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-06 00:54:48 +0000

    Added domain defs
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@42 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -11,4 +11,14 @@ $CI_DATABASE      = 'ci';
 $CI_FORUM_HOME    = '/var/www/ci4/forum/';
 $CI_FORUM_ADDRESS = $CI_ADDRESS . '/forum';
 
+/* Domain definitions
+ * These are the column names in the ciforum database
+ */
+$CI_DOMAIN{1}     = 'field14';
+$CI_DOMAIN{2}     = 'field15';
+$CI_DOMAIN{3}     = 'field16';
+$CI_DOMAIN{4}     = 'field17';
+$CI_DOMAIN{5}     = 'field18';
+$CI_DOMAIN{6}     = 'field19';
+
 ?>
