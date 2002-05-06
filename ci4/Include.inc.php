f12a967a07342bd3a74c01243dac0fc43bb3a039
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-06 01:01:55 +0000

    Initial add
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@43 157e5a38-50d5-0310-a571-dcc21a78b580

---
+++ ci4/Include.inc.php
@@ -0,0 +1,26 @@
+<?
+
+require_once $CI_HOME_MOD . 'config/Globals.inc.php';
+
+// Requires
+require_once $CI_HOME . 'config/Database.inc.php';
+
+require_once $CI_HOME . 'utility/DatabaseAccess.inc.php';
+require_once $CI_HOME . 'utility/Database.inc.php'; // needs to be after DatabaseAccess.inc.php
+require_once $CI_HOME . 'utility/SQLFormat.inc.php'; // needs to be after DatabaseAccess.inc.php
+require_once $CI_HOME . 'utility/GameMath.inc.php';
+require_once $CI_HOME . 'utility/URLUtil.inc.php';
+
+require_once $CI_HOME . 'objects/GameObjectUnknown.inc.php';
+require_once $CI_HOME . 'objects/GameObjectEntity.inc.php'; // needs to be after GameObjectUnknown.inc.php
+
+// Setup database connections
+$DB = new Database;
+$DBItem = new Database;
+$DBAuction = new Database
+
+$DB->Connect($CIConfig1, $CI_DATABASE);
+$DBItem->Connect($CIConfig2);
+$DBAuction->Connect($CIConfig3);
+
+?>
