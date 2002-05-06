44fad0122199b51ac5c3ccdeffa72213e4322bff
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-06 00:13:23 +0000

    Changed a few things
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@38 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Database.inc.php
+++ ci4/config/Database.inc.php
@@ -7,12 +7,12 @@ $CIConfig1{"SQLUser"}       = "usersql";
 $CIConfig1{"SQLPassword"}   = "user";
 $CIConfig1{"SQLHost"}       = "localhost";
 
-// Item database
+// Item server
 $CIConfig2{"SQLUser"}       = "usersql";
 $CIConfig2{"SQLPassword"}   = "user";
 $CIConfig2{"SQLHost"}       = "localhost:33062";
 
-// Auctions
+// Auction server
 $CIConfig3{"SQLUser"}       = "usersql";
 $CIConfig3{"SQLPassword"}   = "user";
 $CIConfig3{"SQLHost"}       = "localhost:33063";
