6046659b229b356dfdfcc0d0dc48fd6213952554
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-06 00:12:56 +0000

    Added more vars
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@37 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -2,9 +2,13 @@
 
 /*	CI globals.	*/
 
-$CI_HOME      = "/var/www/ci4";
-$CI_DOMAIN    = "crescentisland.com";
-$CI_PATH      = ""; // on the old site (ci3) this would have been "/ci"
-$CI_ADDRESS   = "http://" . $CI_DOMAIN . $CI_ADDRESS . "/";
+$CI_HOME          = '/var/www/ci4';
+$CI_DOMAIN        = 'crescentisland.com';
+$CI_PATH          = ''; // on the old site (ci3) this would have been "/ci"
+$CI_ADDRESS       = 'http://' . $CI_DOMAIN . $CI_PATH;
+$CI_DATABASE      = 'ci';
+
+$CI_FORUM_HOME    = '/var/www/ci4/forum/';
+$CI_FORUM_ADDRESS = $CI_ADDRESS . '/forum';
 
 ?>
