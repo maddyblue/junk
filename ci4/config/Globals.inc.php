884ca32526a669b082d3e522f9de2550997fa251
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-06 01:02:25 +0000

    Added CI_FORUM_DATABASE
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@44 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -2,23 +2,24 @@
 
 /*	CI globals.	*/
 
-$CI_HOME          = '/var/www/ci4';
-$CI_DOMAIN        = 'crescentisland.com';
-$CI_PATH          = ''; // on the old site (ci3) this would have been "/ci"
-$CI_ADDRESS       = 'http://' . $CI_DOMAIN . $CI_PATH;
-$CI_DATABASE      = 'ci';
+$CI_HOME           = '/var/www/ci4';
+$CI_DOMAIN         = 'crescentisland.com';
+$CI_PATH           = ''; // on the old site (ci3) this would have been "/ci"
+$CI_ADDRESS        = 'http://' . $CI_DOMAIN . $CI_PATH;
+$CI_DATABASE       = 'ci';
 
-$CI_FORUM_HOME    = '/var/www/ci4/forum/';
-$CI_FORUM_ADDRESS = $CI_ADDRESS . '/forum';
+$CI_FORUM_HOME     = '/var/www/ci4/forum/';
+$CI_FORUM_ADDRESS  = $CI_ADDRESS . '/forum';
+$CI_FORUM_DATABASE = 'ciforum';
 
 /* Domain definitions
  * These are the column names in the ciforum database
  */
-$CI_DOMAIN{1}     = 'field14';
-$CI_DOMAIN{2}     = 'field15';
-$CI_DOMAIN{3}     = 'field16';
-$CI_DOMAIN{4}     = 'field17';
-$CI_DOMAIN{5}     = 'field18';
-$CI_DOMAIN{6}     = 'field19';
+$CI_DOMAIN{1}      = 'field14';
+$CI_DOMAIN{2}      = 'field15';
+$CI_DOMAIN{3}      = 'field16';
+$CI_DOMAIN{4}      = 'field17';
+$CI_DOMAIN{5}      = 'field18';
+$CI_DOMAIN{6}      = 'field19';
 
 ?>
