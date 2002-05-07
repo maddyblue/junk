b749ce917dd7d9529f2785a24f42a93eac7b6ecb
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-07 03:55:44 +0000

    Starting initial work
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@56 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/index.php
+++ ci4/index.php
@@ -1,7 +1,29 @@
 <?
 
+if(!$SECTION) $SECTION = 'MAIN';
 require_once $CI_HOME_MOD . 'Include.inc.php';
 
-print_r($globals);
+// Template
+if($t);
+else if($CI_TEMPLATE)
+	$t = $CI_TEMPLATE;
+else
+	$t = $CI_DEF_TEMPLATE;
+
+$fd = fopen(getTemplateName($t), 'r');
+if(!$fd)
+{
+	$message .= '<p>' . $t . ' template does not exist. Reverting to default.\n';
+	$t = $CI_DEF_TEMPLATE;
+	$fd = fopen(getTemplateName($t), 'r');
+}
+setcookie('CI_TEMPLATE', $t, time() + 604800, $CI_PATH);
+$template = fread($fd, filesize(getTemplateName($t)));
+
+$logged = '>';
+$ret = $DB->Query('SELECT section,type,text,link FROM site where logged ' . $logged . '= 0' order by orderid);
+echo "<pre>";
+print_r($ret);
+echo "</pre>";
 
 ?>
