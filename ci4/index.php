e92c793b3bee778774d7cd63b8ec50ee0ee5533c
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-09 04:11:17 +0000

    Initial menu creation stuff.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@61 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/index.php
+++ ci4/index.php
@@ -27,12 +27,53 @@ for($i = 0; $i < sizeof($ret{'tag'}); $i++)
 {
 	switch($ret{'type'}{$i})
 	{
-		case 'eval' : eval('$repl = "' . $ret{'repl'}{$i} . '";'); break;
+		case 'eval': eval('$repl = "' . $ret{'repl'}{$i} . '";'); break;
 	}
 	$template = str_replace('<CI' . $ret{'tag'}{$i} . '>', $repl, $template);
 }
 
-$ret = $DB->Query('SELECT section,type,text,link FROM site where logged ' . $logged . '= 0 order by orderid');
+$ret = $DB->Query('SELECT tag,type,text,link FROM site where logged ' . $logged . '= 0 order by orderid');
+
+while(preg_match('/<CI[^>]+>/', $template))
+{
+	$tag = '';
+	$insert = '';
+	$pos = strpos($template, '<CI');
+	$pos1 = strpos($template, '>', $pos + 3);
+	$tag = substr($template, $pos + 3, $pos1 - $pos - 3);
+	$pos2 = strpos($template, '</CI', $pos1);
+	$pos3 = $pos2 + 5 + strlen($tag);
+	$insert = substr($template, $pos1 + 1, $pos2 - $pos1 - 1);
+
+	$pos4 = strpos($insert, 'INSERT');
+	$inslen = strlen($insert);
+
+	$ret = $DB->Query('SELECT type,text,link FROM site where logged ' . $logged . '=0 AND tag=' . "'$tag'" . ' ORDER BY orderid');
+	$ret{'link'} = preg_replace('/^\$/', strtolower($SECTION) . '/', $ret{'link'});
+	$ret{'link'} = preg_replace('/^/', $CI_PATH . '/', $ret{'link'});
+	$repl = "";
+	for($i = 0; $i < sizeof($ret{'type'}); $i++)
+	{
+		$pos6 = $pos4;
+		$pos7 = 6;
+		if($i == 0)
+		{
+			$pos6 = 0;
+			$pos7 += $pos4;
+		}
+		if($i == sizeof($ret{'type'}) - 1)
+			$pos7 = $inslen - $pos6;
+		echo "$pos6 - $pos7 - $insert\n";
+		switch($ret{'type'}{$i})
+		{
+			case 'text': $repl .= substr_replace($insert, $ret{'text'}{$i}, $pos6, $pos7); break;
+			case 'link': $repl .= substr_replace($insert, '<a href="' . $ret{'link'}{$i} . '">' . $ret{'text'}{$i} . '</a>', $pos6, $pos7); break;
+		}
+		$template = str_replace('<CI' . $ret{'tag'}{$i} . '>', $repl, $template);
+	}
+
+	$template = substr_replace($template, $repl, $pos, $pos3 - $pos);
+}
 
 echo $template;
 
