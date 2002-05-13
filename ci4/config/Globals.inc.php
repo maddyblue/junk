3ef39e2e3acb2c0d46f48d009b92bafbd4ae0e6b
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2002-05-13 04:54:01 +0000

    Changed variable definitons to use define().
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@72 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -2,26 +2,26 @@
 
 /*	CI globals.	*/
 
-$CI_HOME           = '/var/www/ci4/';
-$CI_DOMAIN         = 'localhost';
-$CI_PATH           = '/ci4';
-$CI_ADDRESS        = 'http://' . $CI_DOMAIN . $CI_PATH;
-$CI_DATABASE       = 'ci4';
-$CI_DEF_TEMPLATE   = 'ci4';
-$CI_TEMPLATE_LOC   = 'templates';
+define('CI_HOME', '/var/www/ci4/');
+define('CI_DOMAIN', 'localhost');
+define('CI_PATH', '/ci4');
+define('CI_ADDRESS', 'http://' . CI_DOMAIN . CI_PATH);
+define('CI_DATABASE', 'ci4');
+define('CI_DEF_TEMPLATE', 'ci4');
+define('CI_TEMPLATE_LOC', 'templates');
 
-$CI_FORUM_HOME     = '/var/www/ci4/forum/';
-$CI_FORUM_ADDRESS  = $CI_ADDRESS . '/forum';
-$CI_FORUM_DATABASE = 'ciforum';
+define('CI_FORUM_HOME', '/var/www/ci4/forum/');
+define('CI_FORUM_ADDRESS', CI_ADDRESS . '/forum');
+define('CI_FORUM_DATABASE', 'ciforum');
 
 /* Domain definitions
  * These are the column names in the ciforum database
  */
-$CI_DOMAIN{1}      = 'field14';
-$CI_DOMAIN{2}      = 'field15';
-$CI_DOMAIN{3}      = 'field16';
-$CI_DOMAIN{4}      = 'field17';
-$CI_DOMAIN{5}      = 'field18';
-$CI_DOMAIN{6}      = 'field19';
+define('CI_DOMAIN1', 'field14');
+define('CI_DOMAIN2', 'field15');
+define('CI_DOMAIN3', 'field16');
+define('CI_DOMAIN4', 'field17');
+define('CI_DOMAIN5', 'field18');
+define('CI_DOMAIN6', 'field19');
 
 ?>
