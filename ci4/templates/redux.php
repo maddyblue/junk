cfc92e119be7f6a5f5e2857c8c3460d4b8449017
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-04-18 04:21:28 +0000

    add ub's redux style, and hack it to work with the new css functions
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@109 157e5a38-50d5-0310-a571-dcc21a78b580

---
+++ ci4/templates/redux.php
@@ -0,0 +1,232 @@
+<?php
+
+define('TEMPLATE_ADDROW_BOTTOM', true);
+define('TEMPLATE_ADDROW_RIGHT', true);
+
+?>
+
+<html>
+<head>
+<style type="text/css">
+<!--
+body {
+  background-color: D5D9DD;
+}
+
+p, body, td, li, input, textarea, select {
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 10px;
+  color: 333333;
+  letter-spacing: 1px;
+}
+
+input.text {
+  width: 285px;
+  background-color: #FFFFFF;
+}
+
+.submit {
+  padding-top: 2px;
+  height: 22 px;
+  width: 88px;
+  color: 224488;
+  font-weight: bold;
+  background-image: url(<CI_TEMPLATE_DIR>/button.gif);
+  background-repeat: no-repeat;
+  cursor: pointer;
+}
+
+blockquote {
+  width: 350px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border-top: 1px solid #000000;
+  border-bottom: 1px solid #000000;
+  margin-left: 15px;
+  margin-right: 15px;
+  font-size: 9px;
+  letter-spacing: normal;
+}
+
+textarea {
+  background-color: #FFFFFF;
+}
+
+a {
+	text-decoration: none;
+	color: 224488;
+
+}
+a:hover {
+	text-decoration: underline;
+}
+
+table.block {
+  border: 1px solid #000000;
+  margin-bottom: 8px;
+}
+
+.nav {
+  width: 175px;
+  border: 1px solid #000000;
+  margin-bottom: 8px;
+  margin-left: 8px;
+  table-layout: fixed;
+}
+
+.nav td {
+  padding: 5px;
+  padding-left: 8px;
+  font-weight: bold;
+  text-transform: lowercase;
+  overflow: hidden;
+}
+
+.nav a {
+	font-weight: normal;
+}
+
+td.block-dark {
+  border-bottom: 1px solid #777777;
+  border-right: 1px solid #777777;
+  border-left: 1px solid #DDDDDD;
+  border-top: 1px solid #DDDDDD;
+  background-color: C0C7CD;
+}
+
+td.block-dark a {
+	font-weight: bold;
+}
+
+td.block-light {
+  border-bottom: 1px solid #888888;
+  border-right: 1px solid #888888;
+  border-left: 1px solid #FFFFFF;
+  border-top: 1px solid #FFFFFF;
+  background-color: EEEEEE;
+}
+
+.table1 {
+	border-spacing: 0px;
+}
+
+.td1 {
+	background-color: #DDDDDD;
+	font-weight: bold;
+	text-align: center;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+}
+
+.td2 {
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+}
+
+.td1topright {
+	background-color: #DDDDDD;
+	font-weight: bold;
+	text-align: center;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-right: 1px solid #000000;
+}
+
+.td2topright {
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-right: 1px solid #000000;
+}
+
+.tdbottom {
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-bottom: 1px solid #000000;
+}
+
+.tdright {
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-right: 1px solid #000000;
+}
+
+.tdbottomright {
+	border: 1px solid #000000;
+}
+
+.preflink {
+  padding-top: 5px;
+  padding-bottom: 5px;
+  text-align: center;
+  height: 22 px;
+  width: 88px;
+  margin-right: 8px;
+  background-image: url(<CI_TEMPLATE_DIR>/button.gif);
+  background-repeat: no-repeat;
+}
+
+.preflink a:hover {
+  text-decoration: none;
+}
+-->
+</style>
+</head>
+<body>
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="580" valign="top">
+			<table width="580" border="0" cellspacing="0" cellpadding="0" class="block">
+				<tr>
+					<td align="right" valign="top" class="block-dark" style="padding-top: 3px; padding-bottom: 8px; padding-right: 8px;">
+						<font style="font-size: 18px">crescent island</font>
+  				</td>
+				</tr>
+				<tr>
+					<td align="left" valign="top" class="block-light" style="padding: 8px">
+						<CICONTENT>
+					</td>
+				</tr>
+			</table>
+		</td>
+		<td width="175" valign="top">
+			<table cellspacing="0" class="nav">
+				<tr>
+					<td class="block-dark">
+						nav
+					</td>
+				</tr>
+				<tr>
+					<td class="block-light">
+						<CINAV>INSERT<br></CINAV><br>
+					</td>
+				</tr>
+			</table>
+			<table cellspacing="0" class="nav">
+				<tr>
+					<td class="block-dark">
+						section menu
+					</td>
+				</tr>
+				<tr>
+					<td class="block-light">
+						<CISECTION_MENU>INSERT<br></CISECTION_MENU><br>
+					</td>
+				</tr>
+			</table>
+			<table cellspacing="0" class="nav">
+				<tr>
+					<td class="block-dark">
+						section nav
+					</td>
+				</tr>
+				<tr>
+					<td class="block-light">
+						<CISECTION_NAV>INSERT<br></CISECTION_NAV><br>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
