d16c89d8262e685ebf7884d955e9d4eb9312a45c
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-06-05 04:14:37 +0000

    add makeImg
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@127 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/utility/Functions.inc.php
+++ ci4/utility/Functions.inc.php
@@ -295,4 +295,9 @@ function makeLink($text, $link, $root = false)
 	return '<a href="' . CI_WWW_PATH . ($root ? '' : CI_SECTION_DIR) . $link . '">' . $text . '</a>';
 }
 
+function makeImg($img, $relative = false)
+{
+	return ($img ? '<img src="' . ($relative ? '' : CI_WWW_PATH) . $link . '">' : '');
+}
+
 ?>
