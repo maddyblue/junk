c7b7a569cccf5fffc03c64be942c95951744a690
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-07-13 23:09:37 +0000

    fixes and new functions
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@194 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/utility/Forum.inc.php
+++ ci4/utility/Forum.inc.php
@@ -43,7 +43,40 @@ function forumLinkLastPost($postid)
 			makeLink('-&gt;', '?a=viewpost&postid=' . $ret[0]['forum_post_id'])
 		);
 	else
-		return 'No posts.';
+		return 'No posts';
+}
+
+function getNavBar($forum)
+{
+	if($forum == 0)
+		return '';
+
+	global $DBMain;
+	$res = $DBMain->Query('select * from forum_forum where forum_forum_id=' . $forum);
+
+	$ret = makeLink($res[0]['forum_forum_name'], '?a=viewforum&forumid=' . $res[0]['forum_forum_id']);
+
+	if($res[0]['forum_forum_parent'] != 0)
+		$ret = getNavBar($res[0]['forum_forum_parent']) . ' &gt; ' . $ret;
+	else
+		$ret = makeLink('Home', '?a=viewforum') . ' &gt; '. $ret;
+	
+	return $ret;
+}
+
+function updateFromPost($post)
+{
+	global $DBMain;
+
+	// find the thread and forum this post is in
+	$res = $DBMain->Query('select forum_post_thread from forum_post where forum_post_id=' . $post);
+	$thread = $res[0]['forum_post_thread'];
+	$res = $DBMain->Query('select forum_thread_forum from forum_thread where forum_thread_id=' . $thread);
+	$forum = $res[0]['forum_thread_forum'];
+
+	// update the last post in the thread and forum; increment the forum thread and post counts
+	$DBMain->Query('update forum_forum set forum_forum_last_post=' . $post . ', forum_forum_threads=forum_forum_threads+1, forum_forum_posts=forum_forum_posts+1 where forum_forum_id=' . $forum);
+	$DBMain->Query('update forum_thread set forum_thread_last_post=' . $post . ' where forum_thread_id=' . $thread);
 }
 
 ?>
