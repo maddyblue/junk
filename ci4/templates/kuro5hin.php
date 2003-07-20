3cac2b22d7d03930d3be80e34b5c0faf07b75f17
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-07-20 17:38:30 +0000

    add the k5 skin. i haven't gotten permission to use this yet.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@206 157e5a38-50d5-0310-a571-dcc21a78b580

---
+++ ci4/templates/kuro5hin.php
@@ -0,0 +1,223 @@
+<html>
+<head>
+<title>crescentisland.com || online tactics gaming</title>
+<style type="text/css">
+<!--
+
+body {
+	background-color: #ffffff;
+}
+
+p, body, td, li, input, textarea, select {
+	font-family: verdana, arial, helvetica, sans-serif;
+	font-size: 10px;
+	color: #222222;
+}
+
+a {
+	color: #003767;
+}
+
+a:visited {
+	color: #4185aa;
+}
+
+a:active {
+	color: #c0c0c0;
+}
+
+.table1, .ciNavTable {
+	border-spacing: 0;
+	border: 0;
+	padding: 0;
+	width: 98%;
+	text-align: center;
+}
+
+.ciNavTable, .ciNavTable a {
+	background-color: #eeeeee;
+	text-decoration: none;
+	color: #000;
+}
+
+.tableMain {
+	border-spacing: 0px;
+	border-collapse: collapse;
+}
+
+.tableHeaderCellL, .tableHeaderCell {
+	padding: 2px;
+	background-color: #eeeeee;
+	font-weight: bold;
+	text-align: center;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+}
+
+.tableHeaderCellR {
+	padding: 2px;
+	background-color: #eeeeee;
+	font-weight: bold;
+	text-align: center;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-right: 1px solid #000000;
+}
+
+.tableCell, .tableCellL, .tableCellTL {
+	padding: 2px;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+}
+
+
+
+.tableCellTR, .tableCellR {
+	padding: 2px;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-right: 1px solid #000000;
+}
+
+.tableCellB, .tableCellBL {
+	padding: 2px;
+	border-left: 1px solid #000000;
+	border-top: 1px solid #000000;
+	border-bottom: 1px solid #000000;
+}
+
+.tableCellBR {
+	padding: 2px;
+	border: 1px solid #000000;
+}
+
+-->
+</style>
+</head>
+
+<body>
+
+<br>
+<a href="http://crescentisland.com"><b>Crescent Island</b></a>
+
+<table class="ciNavTable">
+	<tr>
+		<td style="border: 1px solid #000000;">
+			<CINAV><td style="border-top: 1px solid #000000; border-right: 1px solid #000000;; border-bottom: 1px solid #000000;">INSERT</td></CINAV>
+		</td>
+	</tr>
+</table>
+
+<table class="table1">
+	<tr>
+	<td align="right">
+		<b>we need your support:</b> donate via paypal
+	</td>
+	</tr>
+</table>
+
+<p>
+
+<table border=0 width="98%" align="center" cellpadding=5 cellspacing=0>
+	<tr>
+		<td valign="top" width="20%">
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#000000">
+			<tr>
+				<td width="100%">
+					<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#006699">
+						<tr>
+						<td width="100%">
+							<font face="verdana, arial, helvetica, sans-serif" color="#ffffff" size=2>section menu</font>
+						</td>
+					</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0>
+			<tr>
+				<td>
+					<CISECTION_MENU>INSERT<br></CISECTION_MENU>
+				</td>
+			</tr>
+		</table>
+		<p>
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#000000">
+			<tr>
+				<td width="100%">
+					<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#006699">
+						<tr>
+						<td width="100%">
+							<font face="verdana, arial, helvetica, sans-serif" color="#ffffff" size=2>section nav</font>
+						</td>
+					</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0>
+			<tr>
+				<td>
+					<CISECTION_NAV>INSERT<br></CISECTION_NAV>
+				</td>
+			</tr>
+		</table>
+		<p>
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#000000">
+			<tr>
+				<td width="100%">
+					<table width="100%" border=0 cellpadding=1 cellspacing=0 bgcolor="#006699">
+						<tr>
+						<td width="100%">
+							<font face="verdana, arial, helvetica, sans-serif" color="#ffffff" size=2>skins</font>
+						</td>
+					</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+
+		<table width="100%" border=0 cellpadding=1 cellspacing=0>
+			<tr>
+				<td>
+					<CI_SKIN>
+				</td>
+			</tr>
+		</table>
+		<p>
+
+		</td>
+
+		<td valign="top" width="55%">
+			<table width="100%" border=0 cellpadding=1 cellspacing=0 align="center" bgcolor="#000000">
+				<tr>
+					<td>
+						<table width="100%" border=0 cellpadding=0 cellspacing=0 align="center" bgcolor="#006699">
+							<tr>
+								<td valign="middle">
+									<font face="verdana, arial, helvetica, sans-serif" size="+1" color="#ffffff"><b><?php echo strtolower(CI_SECTION);?></b></font>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+
+			<table width="100%" border=0 cellpadding=3 cellspacing=0>
+				<tr>
+					<td valign="top" width="12" align="left">
+						<CICONTENT>
+					</td>
+				</tr>
+			</table>
+		</td>
+
+	</tr>
+</table>
+
+</body></html>
