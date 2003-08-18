67a4e39696d37697a39cea49d5c3aeadd77f8e07
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-08-18 05:11:27 +0000

    do a full reset for now
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@235 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/forum/reset.php
+++ ci4/forum/reset.php
@@ -4,5 +4,7 @@ $DBMain->Query('truncate table forum_post');
 $DBMain->Query('truncate table forum_thread');
 $DBMain->Query('update forum_forum set forum_forum_threads=0, forum_forum_posts=0, forum_forum_last_post=0');
 $DBMain->Query('update user set user_posts=0');
+$DBMain->Query('truncate table user');
+$DBMain->Query('truncate table forum_forum');
 
 ?>
