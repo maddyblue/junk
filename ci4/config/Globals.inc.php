422910ee54cc961f9302d349cadbbc8804145344
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2003-08-22 04:29:37 +0000

    these are not used
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@257 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Globals.inc.php
+++ ci4/config/Globals.inc.php
@@ -17,7 +17,5 @@ define('CI_WWW_ADDRESS', 'http://' . CI_WWW_DOMAIN . CI_WWW_PATH);
 define('CI_WWW_ADDRESS_HTTPS', 'https://' . CI_WWW_DOMAIN . CI_WWW_PATH);
 
 define('CI_TEMPLATE_FS', CI_FS_PATH . 'templates/');
-define('CI_TEMPLATE_WWW', CI_WWW_PATH . 'templates/');
-define('CI_FORUM_WWW_ADDRESS', CI_WWW_ADDRESS . 'forum/');
 
 ?>
