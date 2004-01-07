d830da66eb80fd7083d60edee9563722c214d761
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-01-07 06:58:07 +0000

    a few style updates, zero functionality changes: " -> ' better no group specified error handling lots of knf Mod -> Supermod (just to be clear) remove the == 1 comparison for the checkboxes, as it is unnecessary
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@428 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/admin/manage-group.php
+++ ci4/admin/manage-group.php
@@ -12,65 +12,73 @@ function groupUserListManage(&$array, $groupid)
 		array_push($array, array(
 			$res[0]['user_name'],
 			makeLink('Remove', 'a=remove-group-user&g=' . $groupid . '&user=' . $row['group_user_user'])
-			));
+		));
 	}
 }
 
-if (isset($_GET['g']))
-	$groupid = $_GET['g'];
-else
-	echo "No group specified";
+$groupid = isset($_GET['g']) ? encode($_GET['g']) : 0;
 
-$res = $DBMain->Query('select * from group_def where group_def_id = ' . $groupid);
+if($groupid)
+{
+	$res = $DBMain->Query('select * from group_def where group_def_id = ' . $groupid);
 
-echo "Manage " . $res[0]['group_def_name'];
+	echo 'Manage ' . $res[0]['group_def_name'];
 
-echo "<p>" . getTableForm('Name Change', array(
+	echo '<p>' .
+		getTableForm('Name Change', array(
 			array('Group Name', array('type'=>'text', 'name'=>'name', 'val'=>$res[0]['group_def_name'])),
 			array('', array('type'=>'submit','name'=>'submit', 'val'=>'Update Name')),
 			array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid)),
-			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>"edit-group"))
-			));
-
-
-echo "<p>" . getTableForm('Group Permissions', array(
-				array('Admin', array('name'=>'admin', 'type'=>'checkbox', 'val'=>($res[0]['group_def_admin'] == 1) ? 'checked' : 'unchecked')),
-				array('News', array('name'=>'news', 'type'=>'checkbox', 'val'=>($res[0]['group_def_news'] == 1) ? 'checked' : 'unchecked')),
-				array('Mod', array('name'=>'mod', 'type'=>'checkbox', 'val'=>($res[0]['group_def_mod'] == 1) ? 'checked' : 'unchecked')),
-				array('Banned', array('name'=>'banned', 'type'=>'checkbox', 'val'=>($res[0]['group_def_banned'] == 1) ? 'checked' : 'unchecked')),
-
-				array('', array('type'=>'submit','name'=>'submit', 'val'=>'Update Permissions')),
-				array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid)),
-				array('', array('type'=>'hidden', 'name'=>'a', 'val'=>"edit-group-perms"))
-				));
-
-$array = array();
-
-array_push($array, array(
-	'Name',
-	'Remove'
-	));
-
-groupUserListManage($array, $groupid);
-
-$res = $DBMain->Query('select group_def_name from group_def where group_def_id=' . $groupid);
-
-echo "<p>Users";
-
-echo getTable($array) . "<p>";
-
-echo getTableForm('Add User to group', array(
-		array('User Name', array('type'=>'text', 'name'=>'name', 'val'=>'')),
-		array('', array('type'=>'submit', 'name'=>'submit', 'val'=>'Add User')),
-		array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid)),
-		array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'add-group-user')),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'edit-group'))
 		));
 
-echo "<p>" . getTableForm('Delete ' . $res[0]['group_def_name'], array(
-		array('', array('type'=>'submit', 'name'=>'submit', 'val'=>'Delete')),
-		array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'delete-group')),
-		array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid))
+
+	echo '<p>' .
+		getTableForm('Group Permissions', array(
+			array('Admin', array('name'=>'admin', 'type'=>'checkbox', 'val'=>($res[0]['group_def_admin']) ? 'checked' : 'unchecked')),
+			array('News', array('name'=>'news', 'type'=>'checkbox', 'val'=>($res[0]['group_def_news']) ? 'checked' : 'unchecked')),
+			array('Supermod', array('name'=>'mod', 'type'=>'checkbox', 'val'=>($res[0]['group_def_mod']) ? 'checked' : 'unchecked')),
+			array('Banned', array('name'=>'banned', 'type'=>'checkbox', 'val'=>($res[0]['group_def_banned']) ? 'checked' : 'unchecked')),
+
+			array('', array('type'=>'submit','name'=>'submit', 'val'=>'Update Permissions')),
+			array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid)),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'edit-group-perms'))
 		));
 
-echo "<p>" . makeLink("Go back to Manage Groups", '?a=manage-groups');
+	$array = array();
+
+	array_push($array, array(
+		'Name',
+		'Remove'
+		));
+
+	groupUserListManage($array, $groupid);
+
+	$res = $DBMain->Query('select group_def_name from group_def where group_def_id=' . $groupid);
+
+	echo '<p>Users';
+
+	echo getTable($array);
+
+	echo '<p>' .
+		getTableForm('Add User to group', array(
+			array('User Name', array('type'=>'text', 'name'=>'name', 'val'=>'')),
+			array('', array('type'=>'submit', 'name'=>'submit', 'val'=>'Add User')),
+			array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid)),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'add-group-user')),
+		));
+
+	echo '<p>' .
+		getTableForm('Delete ' . $res[0]['group_def_name'], array(
+			array('', array('type'=>'submit', 'name'=>'submit', 'val'=>'Delete')),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'delete-group')),
+			array('', array('type'=>'hidden', 'name'=>'g', 'val'=>$groupid))
+		));
+}
+else
+{
+	echo '<p>No group specified.';
+}
+
+echo '<p>' . makeLink('Go back to Manage Groups', '?a=manage-groups');
 ?>
