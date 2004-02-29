c3ca50b160d379311fbc72d0694306a25cc036b0
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-02-29 19:04:52 +0000

    add links for the various messaging sevrices from rayle
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@481 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/user/viewuserdetails.php
+++ ci4/user/viewuserdetails.php
@@ -39,17 +39,27 @@ $res = $DBMain->Query('select * from user where user_id=' . $user);
 if(count($res) == 1)
 {
 	$www = decode($res[0]['user_www']);
+	$www = makeLink($www, $www, 'EXTERIOR');
+
+	$aim = decode($res[0]['user_aim']);
+	$aim = makeLink($aim, 'aim:goim?screenname=' . $aim . '&message=Hello.', 'EXTERIOR');
+
+	$yahoo = decode($res[0]['user_yahoo']);
+	$yahoo = makeLink($yahoo, 'http://edit.yahoo.com/config/send_webmesg?.target=' . $yahoo . '&.src=pg', 'EXTERIOR');
+
+	$icq = decode($res[0]['user_icq']);
+	$icq = makeLink($icq, 'http://wwp.icq.com/' . $icq . '#pager', 'EXTERIOR') . ' - ' . makeLink(makeImg('http://web.icq.com/whitepages/online?icq=' . $icq . '&img=5', '', true), 'http://wwp.icq.com/' . $icq . '#pager', 'EXTERIOR');
 
 	$array = array(
 		array('User', decode($res[0]['user_name'])),
 		array('Avatar', getAvatarImg($res[0]['user_avatar_data'])),
 		array('Register date', getTime($res[0]['user_register'])),
 		array('Forum posts', $res[0]['user_posts']),
-		array('AIM', decode($res[0]['user_aim'])),
-		array('Yahoo', decode($res[0]['user_yahoo'])),
-		array('ICQ', decode($res[0]['user_icq'])),
+		array('AIM', $aim),
+		array('Yahoo', $yahoo),
+		array('ICQ', $icq),
 		array('MSN', decode($res[0]['user_msn'])),
-		array('WWW', makeLink($www, $www, 'EXTERIOR')),
+		array('WWW', $www),
 		array('Signature', parseSig($res[0]['user_sig']))
 	);
 
