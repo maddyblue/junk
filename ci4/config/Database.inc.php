df5551cd0cb365cde37ed80061aa839f5b8dc680
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-08-31 16:07:00 +0000

    we moved again
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@605 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/config/Database.inc.php
+++ ci4/config/Database.inc.php
@@ -6,6 +6,6 @@
 
 $CIConfigMain{'SQLUser'}       = 'ci4';
 $CIConfigMain{'SQLPassword'}   = 'ci4sql';
-$CIConfigMain{'SQLHost'}       = '127.0.0.1';
+$CIConfigMain{'SQLHost'}       = 'localhost';
 
 ?>
