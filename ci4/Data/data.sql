f4c31931ba954ff0e286bfee588f69b6b023b3c4
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-11-15 01:53:47 +0000

    some simple stats
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@654 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/Data/data.sql
+++ ci4/Data/data.sql
@@ -251,6 +251,7 @@ INSERT INTO site VALUES ('GAME_SECTION_NAV',50,'link','View Towns','','\'a=viewt
 INSERT INTO site VALUES ('GAME_SECTION_NAV',60,'link','View Areas','','\'a=viewareas\'','SECTION_GAME',0,0,'');
 INSERT INTO site VALUES ('MAIN_SECTION_NAV',0,'link','Domains','','\'a=domains\'','SECTION_HOME',0,0,'');
 INSERT INTO site VALUES ('MAIN_SECTION_NAV',10,'link','Skins','','\'a=skins\'','SECTION_HOME',0,0,'');
+INSERT INTO site VALUES ('MAIN_SECTION_NAV',20,'link','Stats','','\'a=stats\'','SECTION_HOME',0,0,'');
 INSERT INTO site VALUES ('MANUAL_SECTION_MENU',0,'link','Skinning','','\'a=skinning\'','SECTION_MANUAL',0,0,'');
 INSERT INTO site VALUES ('MANUAL_SECTION_MENU',10,'link','Advanced Skinning','','\'a=skinning-advanced\'','SECTION_MANUAL',0,0,'');
 INSERT INTO site VALUES ('MANUAL_SECTION_MENU',20,'link','IRC','','\'a=irc\'','SECTION_MANUAL',0,0,'');
---
+++ ci4/main/stats.php
@@ -0,0 +1,61 @@
+<?php
+
+/* $Id$ */
+
+/*
+ * Copyright (c) 2004 Matthew Jibson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+$SecPerDay = 86400;
+$SecPerWeek = 604800;
+
+$stats = array(
+	array('Registered users', 'select count(*) from user'),
+	array('New users in the last week', 'select count(*) from user where user_register > (UNIX_TIMESTAMP() - ' . $SecPerWeek . ')'),
+	array('Active users for the past week', 'select count(*) from user where user_last > (UNIX_TIMESTAMP() - ' . $SecPerWeek . ')'),
+	array('Active users for the past day', 'select count(*) from user where user_last > (UNIX_TIMESTAMP() - ' . $SecPerDay . ')'),
+	array('Forum posts', 'select count(*) from forum_post'),
+	array('Forum posts for the past week', 'select count(*) from forum_post where forum_post_date > (UNIX_TIMESTAMP() - ' . $SecPerWeek . ')'),
+	array('Forum posts for the past day', 'select count(*) from forum_post where forum_post_date > (UNIX_TIMESTAMP() - ' . $SecPerDay . ')'),
+	array('Forum threads', 'select count(*) from forum_thread'),
+	array('Registered players', 'select count(*) from player'),
+	array('Battles', 'select count(*) from battle')
+);
+
+$table = array();
+
+foreach($stats as $s)
+{
+	$r = $db->query($s[1]);
+	array_push($table, array($s[0], $r[0]['count(*)']));
+}
+
+echo getTable($table, false);
+
+?>
