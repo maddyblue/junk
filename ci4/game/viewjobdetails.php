249caffb2fcf2e5c815dfb581983fc8a67fd9966
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-12-20 20:30:30 +0000

    display current job name put everything into the standard structure of displaying current player information and checking if the $_GET['job'] id is valid.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@685 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/game/viewjobdetails.php
+++ ci4/game/viewjobdetails.php
@@ -32,169 +32,173 @@
  *
  */
 
-if($PLAYER && isset($_POST['job']))
-{
+$job = isset($_GET['job']) ? intval($_GET['job']) : '0';
+
+if(isset($_POST['job']))
 	$job = intval($_POST['job']);
-
-	$fail = false;
-
-	$res = $db->query('select job_req_lv, job_name from job where job_id=' . $job);
-	if(!count($res))
-	{
-		echo '<br>Unknown job.';
-		$fail = true;
-	}
-
-	if($res[0]['job_req_lv'] > $PLAYER['player_lv'])
-	{
-		echo '<br>You are not yet at a high enough level to change to ' . $res[0]['job_name'] . '.';
-		$fail = true;
-	}
-
-	$failed = $db->query('select job_name, player_job_lv, cor_job_joblv.*
-		from cor_job_joblv, job
-		left join player_job on
-			player_job_player=' . $PLAYER['player_id'] . ' and
-			cor_job_req=player_job_job
-		where cor_job=' . $job . ' and
-			job_id=cor_job_req and
-			(player_job_lv is NULL or
-			player_job_lv < cor_joblv)');
-
-	if(count($failed))
-	{
-		foreach($failed as $entry)
-			echo '<br>You must be ' . $entry['job_name'] . ' level ' . $entry['cor_joblv'] . ', but you are level ' . ($entry['player_job_lv'] ? $entry['player_job_lv'] : '0') . '.';
-		$fail = true;
-	}
-
-	if(!$fail)
-	{
-		$db->query('update player set player_job=' . $job . ' where player_id=' . $PLAYER['player_id']);
-
-		// if this is the first time in this job, add the initial entries
-		$ret = $db->query('select player_job_job from player_job where player_job_player=' . $PLAYER['player_id'] . ' and player_job_job=' . $job);
-		if(count($ret) == 0)
-		{
-			$db->query('insert into player_job values (' . $PLAYER['player_id'] . ', ' . $job . ', 0, 0)');
-
-			$ret = $db->query('select cor_abilitytype from cor_job_abilitytype where cor_job=' . $job);
-			for($i = 0; $i < count($ret); $i++)
-				$db->query('insert into player_abilitytype values (' . $PLAYER['player_id'] . ', ' . $ret[$i]['cor_abilitytype'] . ', 0, 0)');
-		}
-
-		// unequip everything
-		$db->query('update player_equipment set player_equipment_equipped=0 where player_equipment_player=' . $PLAYER['player_id']);
-
-		echo '<p>Job change to ' . $res[0]['job_name'] . ' succeeded.';
-		echo '<br>All of your equipment has been unequipped.';
-	}
-	else
-	{
-		echo '<br>Job change to ' . $res[0]['job_name'] . ' failed.';
-	}
-}
-else
-	$job = isset($_GET['job']) ? intval($_GET['job']) : '0';
 
 $res = $db->query('select * from job where job_id=' . $job);
 
-$equipment = $db->query('select equipmenttype_name, equipmenttype_id from cor_job_equipmenttype, equipmenttype where cor_job=' . $job . ' and equipmenttype.equipmenttype_id=cor_equipmenttype order by equipmenttype_name');
-
-$equipmentlist = '';
-
-if(count($equipment))
+if(count($res))
 {
-	for($i = 0; $i < count($equipment); $i++)
+	if(isset($_POST['job']) && $PLAYER)
 	{
-		if($i) $equipmentlist .= ', ';
-		$equipmentlist .= makeLink($equipment[$i]['equipmenttype_name'], 'a=viewequipment&type=' . $equipment[$i]['equipmenttype_id']);
+
+		$fail = false;
+
+		if($res[0]['job_req_lv'] > $PLAYER['player_lv'])
+		{
+			echo '<br>You are not yet at a high enough level to change to ' . $res[0]['job_name'] . '.';
+			$fail = true;
+		}
+
+		$failed = $db->query('select job_name, player_job_lv, cor_job_joblv.*
+			from cor_job_joblv, job
+			left join player_job on
+				player_job_player=' . $PLAYER['player_id'] . ' and
+				cor_job_req=player_job_job
+			where cor_job=' . $job . ' and
+				job_id=cor_job_req and
+				(player_job_lv is NULL or
+				player_job_lv < cor_joblv)');
+
+		if(count($failed))
+		{
+			foreach($failed as $entry)
+				echo '<br>You must be ' . $entry['job_name'] . ' level ' . $entry['cor_joblv'] . ', but you are level ' . ($entry['player_job_lv'] ? $entry['player_job_lv'] : '0') . '.';
+			$fail = true;
+		}
+
+		if(!$fail)
+		{
+			$db->query('update player set player_job=' . $job . ' where player_id=' . $PLAYER['player_id']);
+
+			// if this is the first time in this job, add the initial entries
+			$ret = $db->query('select player_job_job from player_job where player_job_player=' . $PLAYER['player_id'] . ' and player_job_job=' . $job);
+			if(count($ret) == 0)
+			{
+				$db->query('insert into player_job values (' . $PLAYER['player_id'] . ', ' . $job . ', 0, 0)');
+
+				$ret = $db->query('select cor_abilitytype from cor_job_abilitytype where cor_job=' . $job);
+				for($i = 0; $i < count($ret); $i++)
+					$db->query('insert into player_abilitytype values (' . $PLAYER['player_id'] . ', ' . $ret[$i]['cor_abilitytype'] . ', 0, 0)');
+			}
+
+			// unequip everything
+			$db->query('update player_equipment set player_equipment_equipped=0 where player_equipment_player=' . $PLAYER['player_id']);
+
+			echo '<p>Job change to ' . $res[0]['job_name'] . ' succeeded.';
+			echo '<br>All of your equipment has been unequipped.';
+		}
+		else
+		{
+			echo '<br>Job change to ' . $res[0]['job_name'] . ' failed.';
+		}
 	}
-}
-else
-{
-	$equipmentlist .= 'Cannot equip anything.';
-}
 
-$abilities = $db->query('select abilitytype_name, abilitytype_id from job, abilitytype, cor_job_abilitytype where job_id=cor_job and cor_abilitytype=abilitytype_id and job_id=' . $job);
-$abilitylist = '';
-for($i = 0; $i < count($abilities); $i++)
-{
-	if($i)
-		$abilitylist .= ', ';
+	$equipment = $db->query('select equipmenttype_name, equipmenttype_id from cor_job_equipmenttype, equipmenttype where cor_job=' . $job . ' and equipmenttype.equipmenttype_id=cor_equipmenttype order by equipmenttype_name');
 
-	$abilitylist .= makeLink($abilities[$i]['abilitytype_name'], 'a=viewabilitytypedetails&type=' . $abilities[$i]['abilitytype_id']);
-}
+	$equipmentlist = '';
 
-$jobs = $db->query('select job_name, job_id, cor_joblv from cor_job_joblv, job where cor_job=' . $job . ' and cor_job_req=job.job_id order by job_name');
+	if(count($equipment))
+	{
+		for($i = 0; $i < count($equipment); $i++)
+		{
+			if($i) $equipmentlist .= ', ';
+			$equipmentlist .= makeLink($equipment[$i]['equipmenttype_name'], 'a=viewequipment&type=' . $equipment[$i]['equipmenttype_id']);
+		}
+	}
+	else
+	{
+		$equipmentlist .= 'Cannot equip anything.';
+	}
 
-$joblist = '';
-
-if(count($jobs) == 0)
-{
-	$joblist .= 'None';
-}
-else
-{
-	for($i = 0; $i < count($jobs); $i++)
+	$abilities = $db->query('select abilitytype_name, abilitytype_id from job, abilitytype, cor_job_abilitytype where job_id=cor_job and cor_abilitytype=abilitytype_id and job_id=' . $job);
+	$abilitylist = '';
+	for($i = 0; $i < count($abilities); $i++)
 	{
 		if($i)
-			$joblist .= ', ';
+			$abilitylist .= ', ';
 
-		$joblist .= makeLink($jobs[$i]['job_name'], 'a=viewjobdetails&job=' . $jobs[$i]['job_id']) . ' (' . $jobs[$i]['cor_joblv'] . ')';
+		$abilitylist .= makeLink($abilities[$i]['abilitytype_name'], 'a=viewabilitytypedetails&type=' . $abilities[$i]['abilitytype_id']);
 	}
+
+	$jobs = $db->query('select job_name, job_id, cor_joblv from cor_job_joblv, job where cor_job=' . $job . ' and cor_job_req=job.job_id order by job_name');
+
+	$joblist = '';
+
+	if(count($jobs) == 0)
+	{
+		$joblist .= 'None';
+	}
+	else
+	{
+		for($i = 0; $i < count($jobs); $i++)
+		{
+			if($i)
+				$joblist .= ', ';
+
+			$joblist .= makeLink($jobs[$i]['job_name'], 'a=viewjobdetails&job=' . $jobs[$i]['job_id']) . ' (' . $jobs[$i]['cor_joblv'] . ')';
+		}
+	}
+
+	$stat = array(
+		array('HP', $res[0]['job_stat_hp'] . '%'),
+		array('MP', $res[0]['job_stat_mp'] . '%'),
+		array('STR', $res[0]['job_stat_str'] . '%'),
+		array('MAG', $res[0]['job_stat_mag'] . '%'),
+		array('DEF', $res[0]['job_stat_def'] . '%'),
+		array('MGD', $res[0]['job_stat_mgd'] . '%'),
+		array('AGL', $res[0]['job_stat_agl'] . '%'),
+		array('ACC', $res[0]['job_stat_acc'] . '%')
+	);
+
+	$level = array(
+		array('HP', $res[0]['job_level_hp']),
+		array('MP', $res[0]['job_level_mp']),
+		array('STR', $res[0]['job_level_str']),
+		array('MAG', $res[0]['job_level_mag']),
+		array('DEF', $res[0]['job_level_def']),
+		array('MGD', $res[0]['job_level_mgd']),
+		array('AGL', $res[0]['job_level_agl']),
+		array('ACC', $res[0]['job_level_acc'])
+	);
+
+	// Setup is done, make the table
+
+	$array = array(
+		array('Job', $res[0]['job_name']),
+		array('Description', $res[0]['job_desc']),
+		array('Gender', getGender($res[0]['job_gender'])),
+		array('Required Level', $res[0]['job_req_lv']),
+		array('Wage', $res[0]['job_wage']),
+		array('Useable Equipment Types', $equipmentlist),
+		array('Useable Ability Types', $abilitylist),
+		array('Prerequisite Job Levels', $joblist),
+		array('Battle Stats', getTable($stat, false)),
+		array('Level Up Stats', getTable($level, false)),
+	);
+
+	if($PLAYER)
+	{
+		$change = '<p>' . getForm('', array(
+			array('', array('type'=>'submit', 'name'=>'submit', 'val'=>('Change job to ' . $res[0]['job_name']))),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'viewjobdetails')),
+			array('', array('type'=>'hidden', 'name'=>'job', 'val'=>$job))
+		));
+
+		if(!isset($_POST['job']))
+			echo '<p>You are currently a ' . getDBData('job_name', $PLAYER['player_job'], 'job_id', 'job') . '.';
+	}
+	else
+		$change = '';
+
+	echo $change;
+	echo getTable($array);
+	echo $change;
 }
-
-$stat = array(
-	array('HP', $res[0]['job_stat_hp'] . '%'),
-	array('MP', $res[0]['job_stat_mp'] . '%'),
-	array('STR', $res[0]['job_stat_str'] . '%'),
-	array('MAG', $res[0]['job_stat_mag'] . '%'),
-	array('DEF', $res[0]['job_stat_def'] . '%'),
-	array('MGD', $res[0]['job_stat_mgd'] . '%'),
-	array('AGL', $res[0]['job_stat_agl'] . '%'),
-	array('ACC', $res[0]['job_stat_acc'] . '%')
-);
-
-$level = array(
-	array('HP', $res[0]['job_level_hp']),
-	array('MP', $res[0]['job_level_mp']),
-	array('STR', $res[0]['job_level_str']),
-	array('MAG', $res[0]['job_level_mag']),
-	array('DEF', $res[0]['job_level_def']),
-	array('MGD', $res[0]['job_level_mgd']),
-	array('AGL', $res[0]['job_level_agl']),
-	array('ACC', $res[0]['job_level_acc'])
-);
-
-// Setup is done, make the table
-
-$array = array(
-	array('Job', $res[0]['job_name']),
-	array('Description', $res[0]['job_desc']),
-	array('Gender', getGender($res[0]['job_gender'])),
-	array('Required Level', $res[0]['job_req_lv']),
-	array('Wage', $res[0]['job_wage']),
-	array('Useable Equipment Types', $equipmentlist),
-	array('Useable Ability Types', $abilitylist),
-	array('Prerequisite Job Levels', $joblist),
-	array('Battle Stats', getTable($stat, false)),
-	array('Level Up Stats', getTable($level, false)),
-);
-
-$change = getForm('', array(
-	array('', array('type'=>'submit', 'name'=>'submit', 'val'=>'Change to this job')),
-	array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'viewjobdetails')),
-	array('', array('type'=>'hidden', 'name'=>'job', 'val'=>$job))
-));
-
-if($PLAYER)
-	echo '<p>' . $change;
-
-echo getTable($array);
-
-if($PLAYER)
-	echo '<p>' . $change;
+else
+	echo '<p>Invalid job ID.';
 
 update_session_action(0504);
 
