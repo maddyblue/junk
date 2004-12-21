a3dd47a6f14cb645af9498c92da5bf70c8f303a7
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-12-21 09:25:51 +0000

    support ability learning. note that abilities now have levels, like diablo 2 abilities. the effect fields will have to be updated to support this.
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@693 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/game/viewabilitydetails.php
+++ ci4/game/viewabilitydetails.php
@@ -34,31 +34,90 @@
 
 $ability = isset($_GET['ability']) ? intval($_GET['ability']) : '0';
 
+if(isset($_POST['ability']))
+	$ability = intval($_POST['ability']);
+
 $res = $db->query('select * from ability, abilitytype where abilitytype_id=ability_type and ability_id=' . $ability);
 
-$joblist = $db->query('select * from cor_job_abilitytype, job, abilitytype, ability where cor_job=job_id and cor_abilitytype=abilitytype_id and ability_type=abilitytype_id and ability_id=' . $ability);
-$jobs = '';
-for($i = 0; $i < count($joblist); $i++)
+if(count($res))
 {
-	if($i)
-		$jobs .= ', ';
+	if(isset($_POST['ability']))
+	{
+		$a = $db->query('select * from player_ability where player_ability_player=' . $PLAYER['player_id'] . ' and player_ability_ability=' . $ability);
+		$p = $db->query('select * from player_abilitytype where player_abilitytype_player=' . $PLAYER['player_id'] . ' and player_abilitytype_type=' . $res[0]['ability_type']);
 
-	$jobs .= makeLink($joblist[$i]['job_name'], 'a=viewjobdetails&job=' . $joblist[$i]['job_id']);
+		$level = count($a) ? $a[0]['player_ability_level'] : 0;
+		$cost = $res[0]['ability_ap_cost_init'] + $res[0]['ability_ap_cost_level'] * $level;
+
+		if(!count($p))
+			echo '<p>You do note have any AP in ' . $res[0]['abilitytype_name'] . '.';
+		else if($p[0]['player_abilitytype_ap'] < $cost)
+			echo '<p>You only have ' . $p[0]['player_abilitytype_ap'] . ' of the needed ' . $cost . ' AP to learn ' . $res[0]['ability_name'] . '.';
+		else
+		{
+			if(count($a))
+				$db->query('update player_ability set player_ability_level=player_ability_level+1 where player_ability_ability=' . $ability . ' and player_ability_player=' . $PLAYER['player_id']);
+			else
+				$db->query('insert into player_ability (player_ability_player, player_ability_ability, player_ability_level) values (' . $PLAYER['player_id'] . ', ' . $ability . ', 1)');
+
+			$db->query('update player_abilitytype set player_abilitytype_ap=player_abilitytype_ap - ' . $cost . ' where player_abilitytype_type=' . $res[0]['ability_type'] . ' and player_abilitytype_player=' . $PLAYER['player_id']);
+
+			echo '<p>Learned ' . $res[0]['ability_name'] . '.';
+		}
+	}
+
+	$joblist = $db->query('select * from cor_job_abilitytype, job, abilitytype, ability where cor_job=job_id and cor_abilitytype=abilitytype_id and ability_type=abilitytype_id and ability_id=' . $ability);
+	$jobs = '';
+	for($i = 0; $i < count($joblist); $i++)
+	{
+		if($i)
+			$jobs .= ', ';
+
+		$jobs .= makeLink($joblist[$i]['job_name'], 'a=viewjobdetails&job=' . $joblist[$i]['job_id']);
+	}
+
+	// Setup is done, make the table
+
+	$array = array(
+		array('Ability', $res[0]['ability_name']),
+		array('Type', makeLink($res[0]['abilitytype_name'], 'a=viewabilitytypedetails&type=' . $res[0]['abilitytype_id'])),
+		array('Description', $res[0]['ability_desc']),
+		array('Effect', $res[0]['ability_effect']),
+		array('Jobs that can learn this ability', $jobs),
+		array('Required Level', $res[0]['ability_req_lv']),
+		array('AP cost', $res[0]['ability_ap_cost_init'] . '+' . $res[0]['ability_ap_cost_level'] . '/level')
+	);
+
+	if($PLAYER)
+	{
+		$a = $db->query('select * from player_ability where player_ability_player=' . $PLAYER['player_id'] . ' and player_ability_ability=' . $ability);
+
+		if(count($a))
+		{
+			$level = $a[0]['player_ability_level'] + 1;
+			echo '<p>You currently know ' . $res[0]['ability_name'] . ' level ' . $a[0]['player_ability_level'] . '.';
+		}
+		else
+		{
+			$level = 1;
+			echo '<p>You do not know ' . $res[0]['ability_name'] . '.';
+		}
+
+		$learn = '<p>' . getForm('', array(
+			array('', array('type'=>'submit', 'name'=>'submit', 'val'=>('Learn ' . $res[0]['ability_name'] . ' level ' . $level))),
+			array('', array('type'=>'hidden', 'name'=>'a', 'val'=>'viewabilitydetails')),
+			array('', array('type'=>'hidden', 'name'=>'ability', 'val'=>$ability))
+		));
+	}
+	else
+		$learn = '';
+
+	echo $learn;
+	echo getTable($array);
+	echo $learn;
 }
-
-// Setup is done, make the table
-
-$array = array(
-	array('Ability', $res[0]['ability_name']),
-	array('Type', makeLink($res[0]['abilitytype_name'], 'a=viewabilitytypedetails&type=' . $res[0]['abilitytype_id'])),
-	array('Description', $res[0]['ability_desc']),
-	array('Effect', $res[0]['ability_effect']),
-	array('Jobs that can learn this ability', $jobs),
-	array('Required Level', $res[0]['ability_req_lv']),
-	array('AP cost', $res[0]['ability_ap_cost'])
-);
-
-echo getTable($array);
+else
+	echo '<p>Invalid ability ID.';
 
 update_session_action(0501);
 
