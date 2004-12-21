1115565d070c494f1ad7055933343a94cf8017c5
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2004-12-21 01:13:09 +0000

    if a player is viewing this, show their current/total ap for each type
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@689 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/game/viewabilitytypes.php
+++ ci4/game/viewabilitytypes.php
@@ -32,9 +32,6 @@
  *
  */
 
-$query = 'select * from abilitytype order by abilitytype_name';
-$res = $db->query($query);
-
 $array = array();
 
 array_push($array, array(
@@ -42,12 +39,29 @@ array_push($array, array(
 	'Description'
 ));
 
+if($PLAYER)
+{
+	array_push($array[0], 'Current AP', 'Total AP');
+	$query = 'select * from abilitytype
+		left join player_abilitytype on player_abilitytype_type=abilitytype_id and player_abilitytype_player=' . $PLAYER['player_id'] . '
+		order by abilitytype_name';
+}
+else
+	$query = 'select * from abilitytype order by abilitytype_name';
+
+$res = $db->query($query);
+
 for($i = 0; $i < count($res); $i++)
 {
-	array_push($array, array(
+	$a = array(
 		makeLink($res[$i]['abilitytype_name'], 'a=viewabilitytypedetails&type=' . $res[$i]['abilitytype_id']),
 		$res[$i]['abilitytype_desc']
-	));
+	);
+
+	if($PLAYER)
+		array_push($a, $res[$i]['player_abilitytype_ap'], $res[$i]['player_abilitytype_aptot']);
+
+	array_push($array, $a);
 }
 
 echo getTable($array);
