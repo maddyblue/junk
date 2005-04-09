c11ec01304bfd4c3bcd10da0a8d9964c798c1095
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-04-09 21:14:15 +0000

    factor out the actionlist since other things will likely start needing it
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@756 157e5a38-50d5-0310-a571-dcc21a78b580

---
+++ ci4/utility/ActionList.inc.php
@@ -0,0 +1,107 @@
+<?php
+
+/* $Id$ */
+
+/*
+ * Copyright (c) 2005 Matthew Jibson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/* Numbers in this array are of type XXYY. XX is the section code, YY is the script code.
+ * Section codes:
+ * 01: main
+ * 02: admin
+ * 03: user
+ * 04: forum
+ * 05: game
+ * 06: manual
+ * 07: player
+ */
+
+$actionlist = array(
+
+// special
+array(0000, '\'Unknown\''),
+
+// main
+array(0101, 'makeLink(\'Viewing the news\', \'a=news\', SECTION_HOME)'),
+array(0102, 'makeLink(\'Viewing the skins page\', \'a=skins\', SECTION_HOME)'),
+array(0103, 'makeLink(\'Viewing the domains page\', \'a=domains\', SECTION_HOME)'),
+array(0104, 'makeLink(\'Changing their domain\', \'a=domains\', SECTION_HOME)'),
+
+// admin
+array(0200, '\'In the Admin CP\''),
+
+// user
+array(0301, 'makeLink(\'Viewing Who\\\'s online\', \'a=whosonline\', SECTION_USER)'),
+array(0302, '\'Logging in\''),
+array(0303, '\'Logging out\''),
+array(0304, 'makeLink(\'Viewing their remote information\', \'a=info\', SECTION_USER)'),
+array(0305, '\'Registering a new user\''),
+array(0306, 'makeLink(\'Sending a PM\', \'a=sendpm\', SECTION_USER)'),
+array(0307, 'makeLink(\'Veiwing their User CP\', \'a=usercp\', SECTION_USER)'),
+array(0308, 'makeLink(\'Viewing their PMs\', \'a=viewpms\', SECTION_USER)'),
+array(0309, 'makeLink(\'Viewing details of \' . decode(getDBData(\'user_name\', $d)), \'a=viewuserdetails&user=\' . $d, SECTION_USER)'),
+array(0310, 'makeLink(\'Viewing the user list\', \'a=viewusers\', SECTION_USER)'),
+
+// forum
+array(0401, 'makeLink(\'Editing a post\', \'a=viewpost&p=\' . $d, SECTION_FORUM)'),
+array(0402, 'makeLink(\'Replying to thread \' . decode(getDBData(\'forum_thread_title\', $d, \'forum_thread_id\', \'forum_thread\')), \'a=viewthread&t=\' . $d, SECTION_FORUM)'),
+array(0403, 'makeLink(\'Creating a new thread\', \'a=viewforum&f=\' . $d, SECTION_FORUM)'),
+array(0404, 'makeLink(\'Viewing the taglist\', \'a=taglist\', SECTION_FORUM)'),
+array(0405, 'makeLink(\'Viewing the \' . ($d == \'0\' ? \'forums\' : decode(getDBData(\'forum_forum_name\', $d, \'forum_forum_id\', \'forum_forum\')) . \' forum\'), \'a=viewforum&f=\' . $d, SECTION_FORUM)'),
+array(0406, 'makeLink(\'Viewing thread \' . decode(getDBData(\'forum_thread_title\', $d, \'forum_thread_id\', \'forum_thread\')), \'a=viewthread&t=\' . $d, SECTION_FORUM)'),
+array(0407, 'makeLink(\'Viewing the smilies\', \'a=smilies\', SECTION_FORUM)'),
+
+// game
+array(0501, 'makeLink(\'Viewing Abilities\', \'a=viewabilities\', SECTION_GAME)'),
+array(0502, 'makeLink(\'Viewing Areas\', \'a=viewareas\', SECTION_GAME)'),
+array(0503, 'makeLink(\'Viewing Equipment\', \'a=viewequipment\', SECTION_GAME)'),
+array(0504, 'makeLink(\'Viewing Jobs\', \'a=viewjobs\', SECTION_GAME)'),
+array(0505, 'makeLink(\'Viewing Monsters\', \'a=viewmonsters\', SECTION_GAME)'),
+array(0506, 'makeLink(\'Viewing Towns\', \'a=viewtowns\', SECTION_GAME)'),
+array(0507, 'makeLink(\'Viewing Houses\', \'a=viewhouses\', SECTION_GAME)'),
+array(0551, 'makeLink(\'Changing Jobs\', \'a=changejobs\', SECTION_GAME)'),
+
+// manual
+array(0601, 'makeLink(\'Viewing the basic skinning tutorial\', \'a=skinning\', SECTION_MANUAL)'),
+array(0602, 'makeLink(\'Viewing the advanced skinning tutorial\', \'a=skinning-advanced\', SECTION_MANUAL)'),
+array(0603, 'makeLink(\'Viewing the IRC manual page\', \'a=irc\', SECTION_MANUAL)'),
+array(0604, 'makeLink(\'Viewing the how to help page\', \'a=help\', SECTION_MANUAL)'),
+array(0605, 'makeLink(\'Reading about CI\', \'a=about\', SECTION_MANUAL)'),
+array(0606, 'makeLink(\'Reading about the CI Staff\', \'a=staff\', SECTION_MANUAL)'),
+
+// player
+array(0701, 'makeLink(\'Viewing the player list\', \'a=viewplayers\', SECTION_PLAYER)'),
+array(0702, 'makeLink(\'Viewing details of \' . decode(getDBData(\'player_name\', $d, \'player_id\', \'player\')), \'a=viewplayerdetails&player=\' . $d, SECTION_PLAYER)'),
+array(0703, '\'Registering a new player\''),
+array(0704, 'makeLink(\'Managing their abilities\', \'a=abilities\', SECTION_PLAYER)')
+
+);
+
+?>
