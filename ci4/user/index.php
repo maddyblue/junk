28069ce50abf89c52d7dcda2c88142679b5f5964
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-04-09 22:07:35 +0000

    default to my info
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@762 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/user/index.php
+++ ci4/user/index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $Id: index.php,v 1.3 2003/09/25 23:57:35 dolmant Exp $ */
+/* $Id$ */
 
 /*
  * Copyright (c) 2003 Matthew Jibson
@@ -34,6 +34,10 @@
 
 define('CI_SECTION', 'USER');
 define('CI_HOME_MOD', '../');
+
+if(!isset($_GET['a']) && !isset($_POST['a']))
+	$_GET['a'] = 'viewuserdetails';
+
 require '../index.php';
 
 ?>
