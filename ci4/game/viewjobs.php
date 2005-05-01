e4d7332bfe7547b24350eb6400982fd3dca1c323
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-05-01 20:53:29 +0000

    show available jobs
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@847 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/game/viewjobs.php
+++ ci4/game/viewjobs.php
@@ -35,11 +35,21 @@
 /* Get each job, as well as all required jobs for that job.
  * Note that column j is this job, column job is the prereq.
  */
-$query = 'SELECT j.job_name as jname, j.job_id as jid, j.job_gender as jgen, job.job_id, job.job_name, cor_joblv
-FROM job j
+
+$query = 'SELECT j.job_name as jname, j.job_id as jid, j.job_gender as jgen, job.job_id, job.job_name, cor_joblv';
+
+if($PLAYER)
+	$query .= ', player_job_lv';
+
+$query .= ' FROM job j
 LEFT JOIN cor_job_joblv ON cor_job = j.job_id
-LEFT JOIN job ON cor_job_req = job.job_id
-ORDER BY j.job_name, cor_joblv, job.job_name';
+LEFT JOIN job ON cor_job_req = job.job_id';
+
+if($PLAYER)
+	$query .= ' LEFT JOIN player_job ON player_job_player=' . $PLAYER['player_id'] . ' AND
+		cor_job_req=player_job_job';
+
+$query .= ' ORDER BY j.job_name, cor_joblv, job.job_name';
 $res = $db->query($query);
 
 $array = array();
@@ -54,6 +64,7 @@ $reqs = '';
 
 for($i = 0; $i < count($res); $i++)
 {
+
 	// if this row's job is the same as last row, build more of the job req list
 	if($i > 0 && $res[$i]['jid'] == $res[$i - 1]['jid'])
 	{
@@ -62,14 +73,19 @@ for($i = 0; $i < count($res); $i++)
 	// we are the first of this job type
 	else
 	{
+		$available = '* ';
+
 		$reqs = $res[$i]['job_id'] ? makeLink($res[$i]['job_name'], 'a=viewjobdetails&job=' . $res[$i]['job_id']) . ' (' . $res[$i]['cor_joblv'] . ')' : 'None';
 	}
+
+	if($res[$i]['player_job_lv'] < $res[$i]['cor_joblv'])
+		$available = '';
 
 	// if the next job is _not_ the same as this one, add ourselves
 	if($i == count($res) - 1 || $res[$i + 1]['jid'] != $res[$i]['jid'])
 	{
 		array_push($array, array(
-			makeLink($res[$i]['jname'], 'a=viewjobdetails&job=' . $res[$i]['jid']),
+			$available . makeLink($res[$i]['jname'], 'a=viewjobdetails&job=' . $res[$i]['jid']),
 			getGender($res[$i]['jgen']),
 			$reqs
 		));
@@ -81,6 +97,9 @@ if($PLAYER)
 	echo '<p/>You are currently a ' . getDBData('job_name', $PLAYER['player_job'], 'job_id', 'job') . '.';
 
 echo getTable($array);
+
+if($PLAYER)
+	echo '<p/>A * next to the job name means your player can change to that job.';
 
 update_session_action(504);
 
