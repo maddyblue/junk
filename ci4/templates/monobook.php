6ba44f7e2f5e3e993afe2859672158dc368fbd13
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-05-03 23:19:01 +0000

    show battle status in playerlist
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@871 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/templates/monobook.php
+++ ci4/templates/monobook.php
@@ -65,7 +65,7 @@
 							if($pms)
 								echo '<li class="usermessage">' . $pms . '</li>';
 
-							$res = $db->query('select player_name, player_lv, player_id, domain_id, domain_abrev from player, domain where player_user=' . ID . ' and player_domain=domain_id');
+							$res = $db->query('select player_name, player_lv, player_id, player_battle, domain_id, domain_abrev from player, domain where player_user=' . ID . ' and player_domain=domain_id');
 							for($i = 0; $i < count($res); $i++)
 							{
 								echo '<li';
@@ -73,7 +73,12 @@
 								if($res[$i]['player_id'] == $PLAYER['player_id'])
 									echo ' class="usermessage"';
 
-								echo '>' . makeLink(decode($res[$i]['player_name']), 'a=viewplayerdetails&player=' . $res[$i]['player_id'], SECTION_GAME) . ' (' . $res[$i]['player_lv'] . ') [' . makeLink($res[$i]['domain_abrev'], $_SERVER['QUERY_STRING'] . '&domain=' . $res[$i]['domain_id']) . ']</li>';
+								echo '>' . makeLink(decode($res[$i]['player_name']), 'a=viewplayerdetails&player=' . $res[$i]['player_id'], SECTION_GAME);
+
+								if($res[$i]['player_battle'])
+									echo '*';
+
+								echo ' (' . $res[$i]['player_lv'] . ') [' . makeLink($res[$i]['domain_abrev'], $_SERVER['QUERY_STRING'] . '&domain=' . $res[$i]['domain_id']) . ']</li>';
 							}
 						?>
 						</ul>
