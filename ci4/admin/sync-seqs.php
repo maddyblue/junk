f9159dad7cfedddf0f2dbb23631804ded68fbd09
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-08-01 04:59:47 +0000

    quick script to quickly regen sequences
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@900 157e5a38-50d5-0310-a571-dcc21a78b580

---
+++ ci4/admin/sync-seqs.php
@@ -0,0 +1,49 @@
+<?php
+
+/* $Id$ */
+
+/*
+ * Copyright (c) 2005 Matthew Jibson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+$ret = $db->query("select c.relname as seqname from pg_catalog.pg_class c, pg_catalog.pg_namespace n where c.relnamespace=n.oid and c.relkind = 'S' and n.nspname='public'");
+
+foreach($ret as $s)
+{
+	$n = substr($s['seqname'], 0, -7);
+	$len = strlen($n) - 1;
+	$n = substr($n, 0, $len / 2);
+	$m = $n == 'user' ? 'users' : $n;
+	$u = $n == 'battle_entity' ? 'u' : '';
+
+	$db->query("select setval('${m}_${n}_${u}id_seq', max(${n}_id)) from $m");
+	echo "<br/>$m";
+}
+
+?>
