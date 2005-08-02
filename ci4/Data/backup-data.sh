0092e00d58c8f8d5d99ebbc6a96bfa851480871c
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2005-08-02 00:36:07 +0000

    reorder to accomodate for foreign keys
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@903 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/Data/backup-data.sh
+++ ci4/Data/backup-data.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+group1='abilitytype area domain equipmentclass equipmenttype event group_def house item job monstertype site skin town'
+group2='ability equipment monster'
+group3='cor_area_monster cor_area_town cor_job_abilitytype cor_job_equipmenttype cor_job_joblv cor_monster_drop'
+
+all="${group1} ${group2} ${group3}"
+rev="${group3} ${group2} ${group1}"
+
 o=data.sql
 rm -f $o
 
-for i in ability abilitytype area cor_area_monster cor_area_town cor_job_abilitytype cor_job_equipmenttype cor_job_joblv cor_monster_drop domain equipment equipmentclass equipmenttype event group_def house item job monster monstertype site skin town
+for i in $rev
 do
-	echo "truncate table ${i};" >> $o
+	echo "delete from ${i};" >> $o
+done
+
+for i in $all
+do
 	pg_dump -a -O -x -U dolmant -t $i ci4 >> $o
-	echo "select setval('${i}_${i}_id_seq', max(${i}_id)) from $i;" >> $o
 done
