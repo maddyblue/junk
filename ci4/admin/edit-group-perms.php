7f577b0d73901a791e0ca4cd6c17cc8bb97a929e
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2006-06-20 20:28:16 +0000

    no ? in links
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@972 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/admin/edit-group-perms.php
+++ ci4/admin/edit-group-perms.php
@@ -42,9 +42,9 @@ if(isset($_POST['submit']))
 
 	$db->query('update group_def set group_def_admin=' . $admin . ', group_def_news=' . $news . ', group_def_mod=' . $mod . ', group_def_banned=' . $banned . ' where group_def_id=' . $groupid);
 
-	echo 'Permissions Updated<p/>' . makeLink('Go back to Manage Group', '?a=manage-group&g=' . $groupid);
+	echo 'Permissions Updated<p/>' . makeLink('Go back to Manage Group', 'a=manage-group&g=' . $groupid);
 }
 else
-	echo 'Please use ' . makeLink('Manage Groups', '?a=manage-groups') . '.';
+	echo 'Please use ' . makeLink('Manage Groups', 'a=manage-groups') . '.';
 
 ?>
