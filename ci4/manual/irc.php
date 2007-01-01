2db302ecf503353158e33e2c4f81f39aa620e3c9
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2007-01-01 13:20:15 +0000

    no irc atm
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@1043 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/manual/irc.php
+++ ci4/manual/irc.php
@@ -38,48 +38,4 @@ update_session_action(603, '', 'IRC');
 
 <p/><b>IRC</b>
 
-<p/><b>Introduction</b>
-
-<p/>Crescent Island recently re-opened its official IRC channel. To connect, direct your IRC client to:
-
-<p/><tt>irc.dirgotronix.net</tt>
-
-<p/>and join the channel
-
-<p/><tt>#ci</tt>
-
-<p/><b>Huh?</b>
-
-<p/>If you're completely lost, I suggest you read the following document:
-
-<p/><a href="http://www.mirc.com/ircintro.html">http://www.mirc.com/ircintro.html</a>
-
-<p/>The rest of the document assumes you have read and understood this guide.
-
-<p/><b>Rules</b>
-
-<ul>
-<li>No spamming
-<li>No mass flooding of messages
-<li>Keep swearing down to a minimum
-<li>Don't harass other people in the channel
-<li>No bots
-</ul>
-
-<p/>Any operators in the channel have the right to kick and ban anybody at any time for whatever reason.
-
-<p/><b>FAQ</b>
-
-<p/><b>1- Can I make my own channel on the server?</b>
-
-<p/>Sure! Your channel can be created by using <tt>/join #name</tt>, where <tt>#name</tt> is the name of your channel. Your channel name must begin with a pound sign (<tt>#</tt>).
-
-<p/>Your channel can be registered by following the instructions after you type:
-
-<p/><tt>/msg chanserv help register</tt>
-
-<p/><b>2- How can I stop people from using my nick when I'm not online?</b>
-
-<p/>Once you're chatting with a nickname you want to keep, follow the instructions after you type:
-
-<p/><tt>/msg nickserv help register</tt>
+<p/><?php echo ARC_TITLE; ?> does not have an active IRC channel.
