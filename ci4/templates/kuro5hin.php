a728d5fe51ca3e81d2d51d0b3f00d8a28eca49d8
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2007-01-01 02:45:53 +0000

    formatting nits pm displaying
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@1029 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/templates/kuro5hin.php
+++ ci4/templates/kuro5hin.php
@@ -1,146 +1,148 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-	"http://www.w3.org/TR/html4/strict.dtd">
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-<!-- $Id$ -->
-<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-<link rel="alternate" type="application/xml" title="rss" href="<?php echo ARC_WWW_PATH; ?>rss.php"/>
-<title><?php echo strtolower(ARC_TITLE); ?> || <?php echo strtolower(ARC_DESCRIPTION); ?></title>
-<style type="text/css">
-<!--
+	<head>
+	<!-- $Id$ -->
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+	<link rel="alternate" type="application/xml" title="rss" href="<?php echo ARC_WWW_PATH; ?>rss.php"/>
+	<title>
+		<?php echo strtolower(ARC_TITLE); ?> ||
+		<?php echo strtolower(ARC_SECTION); ?>
+		<?php echo $GLOBALS['PAGE_TITLE'] ? '- ' . $GLOBALS['PAGE_TITLE'] : ''; ?>
+	</title>
+	<style type="text/css">
+		<!--
 
-body {
-	background-color: #ffffff;
-}
+		body {
+			background-color: #ffffff;
+		}
 
-p, body, td, li, input, textarea, select {
-	font-family: sans-serif;
-	font-size: 8pt;
-	color: #000000;
-}
+		p, body, td, li, input, textarea, select {
+			font-family: sans-serif;
+			font-size: 8pt;
+			color: #000000;
+		}
 
-a {
-	color: #003767;
-}
+		a {
+			color: #003767;
+		}
 
-a:visited {
-	color: #21659a;
-}
+		a:visited {
+			color: #21659a;
+		}
 
-a:active {
-	color: #c0c0c0;
-}
+		a:active {
+			color: #c0c0c0;
+		}
 
-.ciNavTable, .ciNavTable a {
-	border-spacing: 0px;
-	padding: 0px;
-	text-align: center;
-	width: 100%;
-	background-color: #eeeeee;
-	text-decoration: none;
-	color: #000000;
-}
+		.ciNavTable, .ciNavTable a {
+			border-spacing: 0px;
+			padding: 0px;
+			text-align: center;
+			width: 100%;
+			background-color: #eeeeee;
+			text-decoration: none;
+			color: #000000;
+		}
 
-.tableMain {
-	border-spacing: 0px;
-	border-collapse: collapse;
-}
+		.tableMain {
+			border-spacing: 0px;
+			border-collapse: collapse;
+		}
 
-.tableHeaderCellL, .tableHeaderCell {
-	padding: 2px;
-	background-color: #eeeeee;
-	font-weight: bold;
-	text-align: center;
-	border-left: 1px solid #000000;
-	border-top: 1px solid #000000;
-}
+		.tableHeaderCellL, .tableHeaderCell {
+			padding: 2px;
+			background-color: #eeeeee;
+			font-weight: bold;
+			text-align: center;
+			border-left: 1px solid #000000;
+			border-top: 1px solid #000000;
+		}
 
-.tableHeaderCellR {
-	padding: 2px;
-	background-color: #eeeeee;
-	font-weight: bold;
-	text-align: center;
-	border-left: 1px solid #000000;
-	border-top: 1px solid #000000;
-	border-right: 1px solid #000000;
-}
+		.tableHeaderCellR {
+			padding: 2px;
+			background-color: #eeeeee;
+			font-weight: bold;
+			text-align: center;
+			border-left: 1px solid #000000;
+			border-top: 1px solid #000000;
+			border-right: 1px solid #000000;
+		}
 
-.tableCell, .tableCellL, .tableCellTL {
-	padding: 2px;
-	border-left: 1px solid #000000;
-	border-top: 1px solid #000000;
-}
+		.tableCell, .tableCellL, .tableCellTL {
+			padding: 2px;
+			border-left: 1px solid #000000;
+			border-top: 1px solid #000000;
+		}
 
-.tableCellTR, .tableCellR {
-	padding: 2px;
-	border-left: 1px solid #000000;
-	border-top: 1px solid #000000;
-	border-right: 1px solid #000000;
-}
+		.tableCellTR, .tableCellR {
+			padding: 2px;
+			border-left: 1px solid #000000;
+			border-top: 1px solid #000000;
+			border-right: 1px solid #000000;
+		}
 
-.tableCellB, .tableCellBL {
-	padding: 2px;
-	border-left: 1px solid #000000;
-	border-top: 1px solid #000000;
-	border-bottom: 1px solid #000000;
-}
+		.tableCellB, .tableCellBL {
+			padding: 2px;
+			border-left: 1px solid #000000;
+			border-top: 1px solid #000000;
+			border-bottom: 1px solid #000000;
+		}
 
-.tableCellBR {
-	padding: 2px;
-	border: 1px solid #000000;
-}
+		.tableCellBR {
+			padding: 2px;
+			border: 1px solid #000000;
+		}
 
-td.ciNavTableTd {
-	border-top: 1px solid #000000;
-	border-right: 1px solid #000000;
-	border-bottom: 1px solid #000000;
-}
+		td.ciNavTableTd {
+			border-top: 1px solid #000000;
+			border-right: 1px solid #000000;
+			border-bottom: 1px solid #000000;
+		}
 
-td.box, td.box a {
-	width: 100%;
-	color: #ffffff;
-}
+		td.box, td.box a {
+			width: 100%;
+			color: #ffffff;
+		}
 
-table.box {
-	border-spacing: 0px;
-	padding: 0px;
-	width: 100%;
-	background-color: #000000;
-}
+		table.box {
+			border-spacing: 0px;
+			padding: 0px;
+			width: 100%;
+			background-color: #000000;
+		}
 
-table.boxinner {
-	border-spacing: 0px;
-	padding: 1px;
-	width: 100%;
-	background-color: #006699;
-}
+		table.boxinner {
+			border-spacing: 0px;
+			padding: 1px;
+			width: 100%;
+			background-color: #006699;
+		}
 
-table.content {
-	border-spacing: 3px;
-	padding: 0px;
-	width: 100%;
-	background-color: #ffffff;
-}
+		table.content {
+			border-spacing: 3px;
+			padding: 0px;
+			width: 100%;
+			background-color: #ffffff;
+		}
 
-table.boxcontents {
-	border: 0px;
-	border-spacing: 0px;
-	padding: 1px;
-	width: 100%;
-}
+		table.boxcontents {
+			border: 0px;
+			border-spacing: 0px;
+			padding: 1px;
+			width: 100%;
+		}
 
-table.maintable {
-	border: 0px;
-	border-spacing: 5px;
-	padding: 0px;
-	width: 100%;
-}
+		table.maintable {
+			border: 0px;
+			border-spacing: 5px;
+			padding: 0px;
+			width: 100%;
+		}
 
--->
-</style>
-<ARC_HEAD/>
-</head>
+		-->
+	</style>
+	<ARC_HEAD/>
+	</head>
 
 <body>
 
@@ -185,7 +187,10 @@ table.maintable {
 						<?php
 							$pms = makePMLink();
 							if($pms)
-								echo $pms . '<br/>';
+								echo $pms;
+							else
+								echo '0 new PMs';
+							echo '<br/>';
 
 							$res = $db->query('select player_name, player_id, domain_id, domain_abrev from player, domain where player_user=' . ID . ' and player_domain=domain_id');
 							for($i = 0; $i < count($res); $i++)
