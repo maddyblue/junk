638a3f3411572d4b619eb9fc3a3712e19c252a4e
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2007-01-01 02:45:01 +0000

    $Id$ better pm displaying
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@1028 157e5a38-50d5-0310-a571-dcc21a78b580

--- ci4/templates/monobook.php
+++ ci4/templates/monobook.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
+		<!-- $Id$ -->
 		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1"/>
 		<title>
 			<?php echo strtolower(ARC_TITLE); ?> -
@@ -64,6 +65,8 @@
 							$pms = makePMLink();
 							if($pms)
 								echo '<li class="usermessage">' . $pms . '</li>';
+							else
+								echo '<li>0 new PMs</li>';
 
 							$res = $db->query('select player_name, player_lv, player_id, player_battle, domain_id, domain_abrev from player, domain where player_user=' . ID . ' and player_domain=domain_id');
 							for($i = 0; $i < count($res); $i++)
