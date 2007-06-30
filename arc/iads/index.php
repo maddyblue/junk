62d92fe066a1c221bf308682dcf1bac78aec2b0f
dolmant <dolmant@157e5a38-50d5-0310-a571-dcc21a78b580>
2007-06-30 03:54:29 +0000

    oops, pasto
    git-svn-id: file:///home/mjibson/backup/arcrepo/trunk@1114 157e5a38-50d5-0310-a571-dcc21a78b580

--- arc/iads/index.php
+++ arc/iads/index.php
@@ -18,7 +18,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-define('ARC_SECTION', 'BLOG');
+define('ARC_SECTION', 'IADS');
 define('ARC_HOME_MOD', '../');
 
 require '../index.php';
