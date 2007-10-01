f5082210c26e79f1c2b66b6e1500f132f8c5f8b9
Matt Jibson <matt.jibson@gmail.com>
2007-10-01 01:25:20 -0600

    fix empty result display

--- results/urls.py
+++ results/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls.defaults import *
 from biosensor.results.models import Result
 
-info_dict = {
-'queryset': Result.objects.all()
+r = Result.objects.all()
+
+object_list = {
+	'queryset': r,
+	'allow_empty': True
+}
+
+object_detail = {
+	'queryset': r
 }
 
 urlpatterns = patterns('',
-	(r'^$', 'django.views.generic.list_detail.object_list', info_dict),
-	(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+	(r'^$', 'django.views.generic.list_detail.object_list', object_list),
+	(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', object_detail),
 	(r'^upload/$', 'biosensor.results.views.upload')
 )
