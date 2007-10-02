ce9ae5749f67b37e09439e9b23b2b86fb4948c16
Matt Jibson <matt.jibson@gmail.com>
2007-10-01 19:10:50 -0600

    default path to results

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
 	(r'^results/', include('biosensor.results.urls')),
 	(r'^admin/', include('django.contrib.admin.urls')),
 	(r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'uploads'}),
+	(r'^$', include('biosensor.results.urls')),
 )
