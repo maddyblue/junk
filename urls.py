a101b737dc6850d8fff939d38546330b5ba03b8b
Matt Jibson <matt.jibson@gmail.com>
2007-11-07 23:39:18 -0700

    oops, commit directories of other stuff, too

--- urls.py
+++ urls.py
@@ -2,7 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 	(r'^blog/', include('darc.blog.urls')),
-	(r'^admin/', include('django.contrib.admin.urls')),
+	(r'^login/', include('darc.main.urls')),
+
 	(r'^templates/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'templates'}),
+	(r'^admin/', include('django.contrib.admin.urls')),
 	(r'^$', include('darc.main.urls')),
 )
