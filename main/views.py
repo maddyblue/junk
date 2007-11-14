70a6071c1c035678b1e1aacc39541f4c80a22023
Matt Jibson <matt.jibson@gmail.com>
2007-11-14 03:04:13 -0700

    no errors if the blog is empty

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
-	return render_to_response('main/index.html', {'entry': Blog.objects.all().select_related().order_by('-date')[0], 'entries': Blog.objects.all().order_by('-date')[1:9]})
+	return render_to_response('main/index.html', {'entry': Blog.objects.all().select_related().order_by('-date')[0:1], 'entries': Blog.objects.all().order_by('-date')[1:9]})
 
 class LoginForm(forms.Form):
 	username = forms.CharField()
