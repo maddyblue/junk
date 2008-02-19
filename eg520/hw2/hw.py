9b6008e4381a378056d389d51771f3491d80384a
Matt Jibson <matt.jibson@gmail.com>
2008-02-19 01:22:31 -0700

    most stuff done - i suck at proofs

--- eg520/hw2/hw.py
+++ eg520/hw2/hw.py
@@ -2,27 +2,119 @@ from math import cos, sqrt
 
 def f(n):
 	return n ** 2 + 4 * cos(n)
+#	return n ** 4 - 14 * n ** 3 + 60 * n ** 2 - 70 * n
 
-a = 1
-b = 2
-p = (3 - sqrt(5)) / 2
+def goldensection(a, b, close):
+	p = (3 - sqrt(5)) / 2
+
+	n = 0
+	while (1 - p) ** n > close / (b - a):
+		n += 1
+
+	print 'N: ' + str(n)
+
+	for i in range(n):
+		newa = a + p * (b - a)
+		newb = a + (1 - p) * (b - a)
+		fa = f(newa)
+		fb = f(newb)
+
+		if fa < fb:
+			b = newb
+		else:
+			a = newa
+
+		print '%i & %.4f & %.4f & %.4f & %.4f & [%.4f, %.4f] \\\\' % ((i + 1), newa, newb, fa, fb, a, b)
+
+def fib(n):
+	r = 1
+	s = 0
+
+	while n > 0:
+		n -= 1
+		t = r + s
+		s = r
+		r = t
+
+	return float(r)
+
+def fibonacci(a, b, close):
+	ep = 0.05
+
+	n = 0
+	while (1 + 2 * ep) / (close / (b - a)) > fib(n + 1):
+		n += 1
+
+	print 'N: ' + str(n)
+
+	swap = True
+
+	for i in range(n + 1, 1, -1):
+		p = 1 - fib(i - 1) / fib(i)
+
+		newa = a + p * (b - a)
+		newb = a + (1 - p) * (b - a)
+
+		if i == 2:
+			if swap:
+				newb = a + (1 - p - ep) * (b - a)
+			else:
+				newa = a + (p - ep) * (b - a)
+
+		fa = f(newa)
+		fb = f(newb)
+
+		if fa < fb:
+			swap = False
+			b = newb
+		else:
+			swap = True
+			a = newa
+
+		print '%i & %.4f & %.4f & %.4f & %.4f & %.4f & [%.4f, %.4f] \\\\' % ((n - i + 2), p, newa, newb, fa, fb, a, b)
+
+a = 1.
+b = 2.
 close = 0.2
 
-n = 0
-while (1 - p) ** n > close / (b - a):
-	n += 1
+#goldensection(a, b, close)
+#fibonacci(a, b, close)
 
-print 'N: ' + str(n)
+# Newton's method (problem 9.3)
 
-for i in range(n):
-	newa = a + p * (b - a)
-	newb = a + (1 - p) * (b - a)
-	fa = f(newa)
-	fb = f(newb)
+def g(x1, x2):
+	return [-400. * (x2 - x1 ** 2.) * x1 - 2. * (1 - x1), 200. * (x2 - x1 ** 2.)]
 
-	if fa < fb:
-		b = newb
-	else:
-		a = newa
+def Finv(x1, x2):
+	c = 1. / ((1200. * x1 ** 2. - 400. * x2 + 2.) * 200. - (-400. * x1) * (x1 - 400.))
 
-	print str(i + 1) + ', ' + str(newa) + ', ' + str(newb) + ', ' + str(fa) + ', ' + str(fb) + ', [' + str(a) + ', ' + str(b) + ']'
+	return [
+		[c * 200., c * 400. * x1],
+		[c * (-x1 + 400.), c * (1200. * x1 ** 2. - 400. * x2 + 2.)]
+	]
+
+def Newton(x1, x2):
+	gg = g(x1, x2)
+	ff = Finv(x1, x2)
+	y1 = gg[0] * ff[0][0] + gg[1] * ff[0][1]
+	y2 = gg[0] * ff[1][0] + gg[1] * ff[1][1]
+	newx1 = x1 - y1
+	newx2 = x2 - y2
+	print x1, x2, gg, ff, y1, y2, newx1, newx2
+	return [newx1, newx2]
+
+def fixedstepsize(x1, x2, alpha):
+	gg = g(x1, x2)
+	newx1 = x1 - alpha * gg[0]
+	newx2 = x2 - alpha * gg[1]
+	print x1, x2, gg, newx1, newx2
+	return [newx1, newx2]
+
+x = [0, 0]
+alpha = 0.05
+
+#x = Newton(x[0], x[1])
+#x = Newton(x[0], x[1])
+
+#x = fixedstepsize(x[0], x[1], alpha)
+#x = fixedstepsize(x[0], x[1], alpha)
