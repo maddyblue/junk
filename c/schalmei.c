04873846c4c7a703218ac4215d8ffebfbb6652b2
Matt Jibson <matt.jibson@gmail.com>
2008-12-20 11:14:46 -0700

    wrong return type

--- c/schalmei.c
+++ c/schalmei.c
@@ -191,7 +191,7 @@ int main(int argc, char *argv[])
 	}
 
 	if((f = fopen("out.ck", "w")) == NULL)
-		return NULL;
+		return 1;
 
 	fprintf(f, "%i => int peaks;\n", NUMPEAKS);
 	fprintf(f, "%i => int recs;\n", NUMFILES);
