190679b9c24d505391dd1276fc3b10b99ed386bd
Matt Jibson <matt.jibson@gmail.com>
2009-04-29 03:16:41 -0600

    add some code in the appendix

---
+++ thesis/verbatim/biosensor/models.py
@@ -0,0 +1,188 @@
+from django.conf import settings
+from django.db import models
+from django import forms
+import os
+
+def get_point(time, target):
+  for i in time:
+    if target < i:
+      return time.index(i)
+
+MODE_DIFF = 1
+MODE_HI_LOW = 2
+MODE_CHARACTERIZE = 3
+
+def calc_range(fname, mode=MODE_DIFF, low=0, mid=0, high=0):
+  f = open(fname)
+
+  time = []
+  value = []
+
+  for line in f:
+    l = line.split()
+    time.append(float(l[0]))
+    value.append(float(l[1]))
+
+  f.close()
+
+  if mode == MODE_HI_LOW:
+    minv = min(value)
+    maxv = max(value)
+    mini = value.index(minv)
+    maxi = value.index(maxv)
+    return [[minv, time[mini]], [maxv, time[maxi]]]
+  elif mode == MODE_CHARACTERIZE:
+    i_low = get_point(time, low)
+    i_mid = get_point(time, mid)
+    i_high = get_point(time, high)
+    m = min(value[i_mid:i_high])
+    i_m = value.index(m, i_mid, i_high)
+    slope = (value[i_mid] - value[i_low]) / (time[i_mid] - time[i_low])
+    t = (time[i_m] - time[i_mid]) * slope + value[i_mid]
+    return (t, m, time[i_m])
+
+  idx_min = time.index(min(time)) + 1
+  idx_max = time.index(max(time)) + 1
+
+  if idx_min == len(value):
+    idx = idx_max
+  else:
+    idx = idx_min
+
+  value1 = value[:idx]
+  value2 = value[idx:]
+
+  avg1 = 0
+
+  for i in value1:
+    avg1 = avg1 + i
+
+  avg2 = 0
+
+  for i in value2:
+    avg2 = avg2 + i
+
+  avg1 = avg1 / len(value1)
+  avg2 = avg2 / len(value2)
+
+  return abs(avg1 - avg2)
+
+class Result(models.Model):
+  sensor = models.IntegerField(null=True, blank=True)
+  electrode = models.IntegerField(null=True, blank=True)
+  run_date = models.DateTimeField()
+  upload_date = models.DateTimeField()
+  upload_file = models.FileField(upload_to="uploads")
+  solution = models.CharField(max_length=100, blank=True)
+  notes = models.TextField(max_length=500, blank=True)
+  filename = models.CharField(max_length=100)
+  analysis = models.CharField(max_length=100)
+  init_e = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+  high_e = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+  low_e = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+  init_pn = models.CharField(null=True, blank=True, max_length=1)
+  scan_rate = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=3)
+  sample_interval = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=5)
+  final_e = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+  incr_e = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=4)
+  amplitude = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=4)
+  pulse_width = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=3)
+  sample_width = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=5)
+  pulse_period = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=3)
+  sensitivity = models.DecimalField(null=True, blank=True, max_digits=12, decimal_places=11)
+  range_all = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  range_p2  = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  range_p1  = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  use = models.BooleanField(null=True, default=True)
+  high_val = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  low_val = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  high_time = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=16)
+  low_time = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=16)
+  characterize = models.BooleanField(null=True, blank=True, default=False)
+  characterize_low = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=4)
+  characterize_mid = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=4)
+  characterize_high = models.DecimalField(null=True, blank=True, max_digits=10, decimal_places=4)
+  characterize_concentration = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  characterize_peak = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  characterize_value = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  characterize_base = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=18)
+  characterize_time = models.DecimalField(null=True, blank=True, max_digits=20, decimal_places=10)
+
+  def analyze(self):
+    name = self.upload_file.name
+    os.popen(settings.PROG_AWK + ' -v analysis="' + self.analysis + '" -f ' + settings.MEDIA_ROOT + 'results/plot.awk ' + name)
+
+    if self.analysis == 'Cyclic Voltammetry':
+      self.range_all = calc_range(name + '.avg')
+      self.range_p2  = calc_range(name + '.-2_2')
+      self.range_p1  = calc_range(name + '.-1_1')
+    elif self.analysis == 'i - t Curve' and self.characterize:
+      r = calc_range(self.upload_file.name + '.avg', MODE_CHARACTERIZE, self.characterize_low, self.characterize_mid, self.characterize_high)
+      self.characterize_peak = r[1]
+      self.characterize_value = r[1] - r[0]
+      self.characterize_base = r[0]
+      self.characterize_time = r[2]
+
+      os.popen('%s -v analysis="%s" -v low=%g -v high=%g -v peak=%g -v base=%g -v ctime=%g -f %sresults/plot.awk %s' %(settings.PROG_AWK, self.analysis, self.characterize_mid, self.characterize_time, self.characterize_peak, self.characterize_base, self.characterize_time, settings.MEDIA_ROOT, name))
+
+    r = calc_range(name + '.avg', MODE_HI_LOW)
+    self.low_val = str(r[0][0])
+    self.low_time = str(r[0][1])
+    self.high_val = str(r[1][0])
+    self.high_time = str(r[1][1])
+
+    if self.analysis == 'Differential Pulse Voltammetry':
+      self.characterize_value = str(r[1][0] - r[0][0])
+
+    self.save()
+    os.popen(settings.PROG_GNUPLOT + ' ' + name + '.plt')
+
+  def __unicode__(self):
+    return self.filename + ': ' + self.run_date.strftime('%d %b %y %H:%M:%S')
+
+  class Admin:
+    pass
+
+ELECTRODE_CHOICES = (
+  (1, 'Common'),
+  (2, 'One'),
+  (3, 'Two')
+)
+
+ELECTRODE_SYSTEM_CHOICES = (
+  (1, 'Four'),
+  (2, 'Three')
+)
+
+class Sensor(models.Model):
+  sensor = models.PositiveSmallIntegerField()
+  electrode_system = models.PositiveSmallIntegerField(choices=ELECTRODE_SYSTEM_CHOICES)
+  ref = models.PositiveSmallIntegerField(choices=ELECTRODE_CHOICES)
+  aux = models.PositiveSmallIntegerField(choices=ELECTRODE_CHOICES)
+  we  = models.PositiveSmallIntegerField(choices=ELECTRODE_CHOICES)
+
+  def __unicode__(self):
+    return str(self.sensor)
+
+  class Admin:
+    pass
+
+class Electrode(models.Model):
+  sensor = models.ForeignKey(Sensor)
+  we = models.PositiveSmallIntegerField()
+  size = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
+  spacing = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
+
+  def __unicode__(self):
+    return "s" + self.sensor.__unicode__() + "w" + str(self.we) + " - size: " + str(self.size) + ", spacing: " + str(self.spacing)
+
+  class Admin:
+    pass
+
+class UploadForm(forms.Form):
+  upload_file = forms.FileField()
+  sensor = forms.IntegerField(required=False)
+  electrode = forms.IntegerField(required=False)
+  solution = forms.CharField(max_length=100, required=False)
+  notes = forms.CharField(max_length=500, widget=forms.Textarea, required=False)
+  use = forms.BooleanField(required=False)
---
+++ thesis/verbatim/biosensor/views.py
@@ -0,0 +1,256 @@
+from django.core.paginator import QuerySetPaginator
+from django.shortcuts import render_to_response, get_object_or_404
+from biosensor.results.models import *
+from biosensor import settings
+from decimal import Decimal
+import datetime
+import re
+import os
+import math
+
+def render(request, template, dict={}):
+  r = Result.objects.all().order_by('-run_date', '-upload_date')
+  p = QuerySetPaginator(r, 50)
+
+  try:
+    page_num = int(request.GET['p'])
+    if page_num < 1:
+      page_num = 1
+    elif page_num > p.num_pages:
+      page_num = p.num_pages
+  except:
+    page_num = 1
+
+  page = p.page(page_num)
+
+  dict['p'] = p
+  dict['page'] = page
+  dict['results'] = result_list(page.object_list)
+  dict['r'] = result_list(r)
+
+  return render_to_response(template, dict)
+
+def result_list(results):
+  result_list = []
+  d = ''
+
+  for res in results:
+    nd = res.run_date.strftime("%d %b %y")
+    if nd != d:
+      d = nd
+      result_list.append([])
+
+    result_list[-1].append(res)
+
+  return result_list
+
+def index(request):
+  return render(request, 'results/base.html')
+
+def electrode(request):
+  return render(request, 'results/electrode.html', {'electrodes': Electrode.objects.all().order_by('sensor', 'we')})
+
+def limit(request):
+  lod = Result.objects.filter(notes__contains='lod = ')
+
+  limits = {}
+
+  for s in lod:
+    for l in s.notes.splitlines():
+      p = l.partition(' = ')
+      if p[0] == 'lod':
+        if s.sensor not in limits:
+          limits[s.sensor] = []
+
+        c = s.solution.partition('M')[0]
+        conc = Decimal(c[:-1])
+
+        if c[-1] == 'm':
+          conc *= Decimal('1e-3')
+        elif c[-1] == 'u':
+          conc *= Decimal('1e-6')
+        else:
+          raise ValueError, 'unknown modifier'
+
+        limits[s.sensor].append((conc, Decimal(p[2])))
+        break
+
+  limlist = []
+  for s, lim in limits.iteritems():
+    f = open('uploads/sensors/limit.%i.dat' %s, 'w')
+
+    for conc, value in lim:
+      limlist.append((s, conc, value))
+      f.write('%s %s\n' %(conc, value))
+
+    f.close()
+
+  os.popen(settings.PROG_GNUPLOT + ' uploads/sensors/limit.plt')
+  limlist.sort(cmp=lambda x, y: cmp(x[1], y[1]), reverse=True)
+
+  return render(request, 'results/limit.html', {'limits': limlist})
+
+def sensor(request):
+  return sensors(request, '')
+
+def sensors(request, rangetype):
+  sensors = Result.objects.filter(use=True)
+
+  count = {}
+  avg = {}
+  stdev = {}
+  sterror = {}
+
+  for s in sensors:
+    for l in s.notes.splitlines():
+      p = l.partition(' = ')
+      if p[0] == 'ip':
+        s.characterize_value = Decimal(p[2])
+        s.save()
+        break
+
+    if s.sensor not in count:
+      count[s.sensor] = 0
+      avg[s.sensor] = 0
+
+    avg[s.sensor] += float(s.characterize_value)
+    count[s.sensor] += 1
+
+  sensors = sensors.order_by('-characterize_value')
+
+  for k in avg.keys():
+    avg[k] /= count[k]
+    l = sensors.filter(sensor=k)
+    vl = list((float(i.characterize_value) - avg[k]) ** 2 for i in l)
+    stdev[k] = math.sqrt(sum(vl) / count[k])
+    sterror[k] = stdev[k] / math.sqrt(count[k])
+
+  senlist = []
+  for k, v in avg.iteritems():
+    senlist.append((k, v))
+  senlist.sort(cmp=lambda x, y: cmp(x[1], y[1]), reverse=True)
+
+  perc = []
+  m = senlist[0][1] / 100
+
+  for (id, v) in senlist:
+    se = sterror[id]
+    perc.append([id, v, v / m, se / m, se])
+
+  return render(request, 'results/sensors.html', {'sensors': sensors, 'perc': perc})
+
+def detail(request, result_id):
+  r = get_object_or_404(Result, pk=result_id)
+
+  try:
+    if request.POST['reanalyze'] == 'reanalyze':
+      if r.analysis == 'i - t Curve':
+        r.characterize = 'characterize' in request.POST and request.POST['characterize'] == 'on'
+        if(request.POST['low']):
+          r.characterize_low = request.POST['low']
+        else:
+          r.characterize_low = None
+        if(request.POST['mid']):
+          r.characterize_mid = request.POST['mid']
+        else:
+          r.characterize_mid = None
+        if(request.POST['high']):
+          r.characterize_high = request.POST['high']
+        else:
+          r.characterize_high = None
+        r.save()
+
+      r.analyze()
+  except KeyError:
+    pass
+
+  try:
+    s = Sensor.objects.get(sensor=r.sensor)
+    e = Electrode.objects.get(sensor=s, we=r.electrode)
+  except:
+    e = None
+
+  return render(request, 'results/detail.html', {'result': r, 'e': e})
+
+def upload(request):
+  months = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'Aug': 8,
+    'Sept': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dec': 12
+  }
+  
+  form = UploadForm()
+
+  if request.method == 'POST' and 'all' in request.POST:
+    res = Result.objects.all()
+
+    for r in res:
+      r.analyze()
+  elif request.method == 'POST':
+    form = UploadForm(request.POST, request.FILES)
+    if form.is_valid():
+      f = request.FILES['upload_file']
+      s = f.read().splitlines()
+      d = re.split('[\., :]+', s[0])
+
+      r = Result(
+        sensor = form.cleaned_data['sensor'],
+        electrode = form.cleaned_data['electrode'],
+        solution = form.cleaned_data['solution'],
+        notes = form.cleaned_data['notes'],
+        upload_date = datetime.datetime.now(),
+        run_date = datetime.datetime(int(d[2]), months[d[0]], int(d[1]), int(d[3]), int(d[4]), int(d[5])),
+        filename = f.name,
+        analysis = s[1],
+        use = form.cleaned_data['use'],
+        init_e = s[8].split(' = ')[1],
+      )
+
+      if s[1] == 'Cyclic Voltammetry':
+        r.high_e = s[9].split(' = ')[1]
+        r.low_e = s[10].split(' = ')[1]
+        r.init_pn = s[11].split(' = ')[1]
+        r.scan_rate = s[12].split(' = ')[1]
+        r.sample_interval = s[14].split(' = ')[1]
+        r.sensitivity = s[16].split(' = ')[1]
+      elif s[1] == 'i - t Curve':
+        r.sample_interval = s[9].split(' = ')[1]
+        r.sensitivity = s[12].split(' = ')[1]
+        r.charaterize = form.cleaned_data['characterize']
+        r.charaterize_low = form.cleaned_data['characterize_low']
+        r.charaterize_mid = form.cleaned_data['characterize_mid']
+        r.charaterize_high = form.cleaned_data['characterize_high']
+        r.characterize_concentration = form.cleaned_data['characterize_concentration']
+      elif s[1] == 'Differential Pulse Voltammetry':
+        r.final_e = s[9].split(' = ')[1]
+        r.incr_e = s[10].split(' = ')[1]
+        r.amplitude = s[11].split(' = ')[1]
+        r.pulse_width = s[12].split(' = ')[1]
+        r.sample_width = s[13].split(' = ')[1]
+        r.pulse_period = s[14].split(' = ')[1]
+        r.sensitivity = s[16].split(' = ')[1]
+
+      if form.cleaned_data['sensor'] is None and len(r.filename) >= 3 and r.filename[0] == 's':
+        r.sensor = r.filename[1:3]
+
+      if form.cleaned_data['electrode'] is None and len(r.filename) >= 6 and r.filename[3] == 'w':
+        r.electrode = r.filename[4:6]
+
+      r.save()
+      r.upload_file.save(str(r.id), f)
+      r.save()
+
+      r.analyze()
+
+      return render(request, 'results/upload.html', {'form': UploadForm(), 'upload': r})
+
+  return render(request, 'results/upload.html', {'form': form})
