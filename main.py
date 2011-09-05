acabbca6d1233fcb9dfc775d3d449759f5664188
Matt Jibson <matt.jibson@gmail.com>
2011-09-05 04:05:19 -0600

    add license and about page

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2011 Matt Jibson <matt.jibson@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 from __future__ import with_statement
 
 import datetime
@@ -175,9 +189,14 @@ class ViewJournal(webapp2.RequestHandler):
 			utils.alert('success', 'Entry posted.')
 			self.redirect(webapp2.uri_for('view-journal', journal=journal))
 
+class AboutHandler(webapp2.RequestHandler):
+	def get(self):
+		rendert(self, 'about.html')
+
 application = webapp2.WSGIApplication([
 	webapp2.Route(r'/', handler=MainPage, name='main'),
 	webapp2.Route(r'/account/', handler=Account, name='account'),
+	webapp2.Route(r'/about/', handler=AboutHandler, name='about'),
 	webapp2.Route(r'/journal/<journal>/<page:\d+>/', handler=ViewJournal, name='view-journal', defaults={'page': 1}),
 	webapp2.Route(r'/login/facebook/', handler=FacebookLogin, name='login-facebook'),
 	webapp2.Route(r'/login/google/', handler=GoogleLogin, name='login-google'),
