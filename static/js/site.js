8464d4182419bce58d87755bd9ad9fd3d94f6277
Matt Jibson <matt.jibson@gmail.com>
2011-09-05 22:25:16 -0600

    use the document ready function since this is loaded at the top

--- static/js/site.js
+++ static/js/site.js
@@ -1,8 +1,10 @@
 // dropdown for topbar nav
-$("body").bind("click", function (e) {
-	$('.dropdown-toggle, .menu').parent("li").removeClass("open");
-});
-$(".dropdown-toggle, .menu").click(function (e) {
-	var $li = $(this).parent("li").toggleClass('open');
-	return false;
+$(function() {
+	$("body").bind("click", function (e) {
+		$('.dropdown-toggle, .menu').parent("li").removeClass("open");
+	});
+	$(".dropdown-toggle, .menu").click(function (e) {
+		var $li = $(this).parent("li").toggleClass('open');
+		return false;
+	});
 });
