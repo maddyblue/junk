39f574058b6715515baf4da2e04efd5780ccc7e5
Matt Jibson <matt.jibson@gmail.com>
2011-09-10 03:18:18 -0600

    File attachment fields for new entry
    Still not hooked up to uploads, but the javascript works. It's probably written poorly, I'm not sure.
    
    Incorporate the speech js into site and just use one file. Perhaps faster?
    
    Django 1.2 adds <p> tags on |linebreaks, so remove ours.

--- static/js/site.js
+++ static/js/site.js
@@ -8,3 +8,58 @@ $(function() {
 		return false;
 	});
 });
+
+// speech support
+$(function() {
+	if(document.createElement('input').webkitSpeech != undefined)
+	{
+		$(".post-speech").before('\
+						<div class="clearfix"> \
+							<label for="speech">speech</label> \
+							<div class="input"> \
+								<input class="xlarge" id="speech" name="speech" type="text" x-webkit-speech /> \
+								<span class="help-inline">click the mic icon and your speech will be added below</span> \
+							</div> \
+						</div> \
+		');
+
+		$("#speech").bind('webkitspeechchange', function() {
+			text = $("#text");
+			speech = $("#speech").val() + ".";
+			speech = speech.substr(0, 1).toUpperCase() + speech.substr(1);
+			if(text.val() != "")
+				text.val(text.val() + " " + speech);
+			else
+				text.val(speech);
+		});
+	}
+});
+
+// file attaching
+// this is probably bad javascript -- improvements are welcome
+$(function() {
+	var attach = '\
+					<div class="clearfix"> \
+						<label for="attach" class="label-attach">attach a file</label> \
+						<div class="input"> \
+							<input class="xlarge file-attach" id="attach" name="attach" type="file" /> \
+							<span id="span-attach" class="help-block">we currently only support images</span> \
+						</div> \
+					</div> \
+	';
+
+	var doattach = function() {
+		$(".file-attach").unbind('change');
+		$(".label-attach").each(function() {
+			$(this).html('<a href="#" onclick="$(this).parent().parent().remove(); return false;">remove</a>');
+		});
+
+		$(".post-attach").before(attach);
+		$(".file-attach").last().change(function() {
+			$("#span-attach").remove();
+			doattach();
+		});
+	}
+
+	doattach();
+});
