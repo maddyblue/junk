32553e5f1aa4c21c2e71aa91b20eca38428f33d2
Matt Jibson <matt.jibson@gmail.com>
2012-06-16 11:27:41 -0400

    Show entries to be deleted on hover

--- static/js/site.js
+++ static/js/site.js
@@ -1,22 +1,36 @@
+function findEvent(n) {
+	var p = $(n);
+	while(!p.hasClass('event'))
+		p = p.parent();
+	return p;
+}
+
+var HIGHLIGHT = 'highlight';
+var map = {};
+
 $(function() {
 	var DEL = '<a href="#" class="del"><i class="icon-remove"></i></a>';
 	$('.title').each(function() {
 		var d = $(DEL);
-		d.data('del', $(this).html());
+		var label = $(this).html();
+		d.data('del', label);
+
+		d.hover(function() {
+			$(map[label]).addClass(HIGHLIGHT);
+		}, function() {
+			$(map[label]).removeClass(HIGHLIGHT);
+		});
+
 		$(this).append(d);
+
+		if(!$(map).attr(label))
+			map[label] = new Array();
+
+		map[label].push(findEvent(this)[0]);
 	});
 
 	$('.del').on("click", function() {
 		var d = $(this).data('del');
-
-		$('.del').each(function() {
-			if($(this).data('del') == d) {
-				var p = $(this);
-				while(!p.hasClass('event'))
-					p = p.parent();
-
-				p.remove();
-			}
-		});
+		$(map[d]).remove();
 	});
 });
