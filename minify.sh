3b1155c9536d8489ec96e0df250946a4a57c383c
Matt Jibson <matt.jibson@gmail.com>
2012-08-14 01:44:32 -0400

    First pass at xing wysihtml5 editor in TNM admin

--- minify.sh
+++ minify.sh
@@ -1,4 +1,4 @@
-cat static/js/jquery.*.js static/js/hallo.js | uglifyjs > static/js/site.min.js
+cat static/js/jquery.*.js | uglifyjs > static/js/site.min.js
 cat static/js/edit.js | uglifyjs > static/js/edit.min.js
 cat static/js/blog.js | uglifyjs > static/js/blog.min.js
 
@@ -11,3 +11,18 @@ done
 
 lessc -x static/css/base.less > static/css/base.css
 lessc -x static/css/blog.less > static/css/blog.css
+lessc -x static/xing-wysihtml5/css/editor.less > static/xing-wysihtml5/css/editor.css
+
+montage -background transparent -tile x1 -geometry +13+7 \
+	static/xing-wysihtml5/sprites/b.png \
+	static/xing-wysihtml5/sprites/i.png \
+	static/xing-wysihtml5/sprites/ul.png \
+	static/xing-wysihtml5/sprites/ol.png \
+	static/xing-wysihtml5/sprites/h1.png \
+	static/xing-wysihtml5/sprites/h2.png \
+	static/xing-wysihtml5/sprites/link.png \
+	static/xing-wysihtml5/sprites/image.png \
+	static/xing-wysihtml5/sprites/left.png \
+	static/xing-wysihtml5/sprites/center.png \
+	static/xing-wysihtml5/sprites/right.png \
+	static/xing-wysihtml5/img/icons.png
---
+++ static/js/xing.js
@@ -0,0 +1,48 @@
+$(function() {
+	$('textarea.editable').each(function() {
+		var toolbardiv = $(
+			'<div id="' + this.id + '-toolbar" class="xing-toolbar" style="display: none;">' +
+				'<header>' +
+					'<ul class="commands">' +
+						'<li data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command"></li>' +
+						'<li data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command"></li>' +
+						'<li data-wysihtml5-command="insertUnorderedList" title="Insert an unordered list" class="command"></li>' +
+						'<li data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command"></li>' +
+						'<li data-wysihtml5-command="createLink" title="Insert a link" class="command"></li>' +
+						'<li data-wysihtml5-command="insertImage" title="Insert an image" class="command"></li>' +
+						'<li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1" title="Insert headline 1" class="command"></li>' +
+						'<li data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command"></li>' +
+						'<li data-wysihtml5-command="justifyLeft" title="Left justify text" class="command"></li>' +
+						'<li data-wysihtml5-command="justifyCenter" title="Center justify text" class="command"></li>' +
+						'<li data-wysihtml5-command="justifyRight" title="Right justify text" class="command"></li>' +
+					'</ul>' +
+				'</header>' +
+				'<div data-wysihtml5-dialog="createLink" style="display: none;">' +
+					'<label>' +
+						'Link: ' +
+						'<input data-wysihtml5-dialog-field="href" value="http://">' +
+					'</label>' +
+					'<a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a>' +
+				'</div>' +
+				'<div data-wysihtml5-dialog="insertImage" style="display: none;">' +
+					'<label>' +
+						'Image: ' +
+						'<input data-wysihtml5-dialog-field="src" value="http://">' +
+					'</label>' +
+					'<a data-wysihtml5-dialog-action="save">OK</a>&nbsp;<a data-wysihtml5-dialog-action="cancel">Cancel</a>' +
+				'</div>' +
+			'</div>');
+
+		toolbardiv.offset({top: -33, left: 0});
+		toolbardiv.width($(this).outerWidth());
+
+		$(this).before(toolbardiv);
+		$(toolbardiv).wrap('<div style="position: relative;" />');
+
+		var editor = new wysihtml5.Editor(this.id, {
+			toolbar: toolbardiv[0].id,
+			stylesheets: ['http://yui.yahooapis.com/2.9.0/build/reset/reset-min.css', '/static/xing-wysihtml5/css/editor.css'],
+			parserRules: wysihtml5ParserRules
+		});
+	});
+});
