67aac22794828452ce939aa2e953bbda69b0d3a5
Matt Jibson <matt.jibson@gmail.com>
2012-10-18 23:44:40 -0400

    Add default sdk email to mini profiler

--- appengine_config.py
+++ appengine_config.py
@@ -48,7 +48,10 @@ def from_fieldstorage(cls, fs):
 multidict.MultiDict.from_fieldstorage = classmethod(from_fieldstorage)
 
 import gae_mini_profiler.profiler
-gae_mini_profiler_ENABLED_PROFILER_EMAILS = ['matt.jibson@gmail.com']
+gae_mini_profiler_ENABLED_PROFILER_EMAILS = [
+	'matt.jibson@gmail.com',
+	'test@example.com',
+]
 
 def webapp_add_wsgi_middleware(app):
 	from google.appengine.ext.appstats import recording
