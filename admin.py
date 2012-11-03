af81c5d5c8c7c59fc303f612f0b0982ed6a36704
Matt Jibson <matt.jibson@gmail.com>
2012-11-02 23:27:30 -0400

    Refactor into multiple files

---
+++ admin.py
@@ -0,0 +1,342 @@
+# Copyright (c) 2012 Matt Jibson <matt.jibson@gmail.com>
+
+import datetime
+import logging
+import os
+
+from PIL import Image
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext import ndb
+from google.appengine.ext import webapp
+import webapp2
+
+from main import BaseHandler, BaseUploadHandler
+import models
+
+class Clear(BaseHandler):
+	@ndb.toplevel
+	def get(self):
+		if not self.request.get('sure'):
+			self.response.write('<html><body><form>clear everything<input type="checkbox" name="sure"><input type="submit"></form></body></html>')
+		else:
+			MODELS = [
+				'BlogPost',
+				'Image',
+				'ImageBlob',
+				'Page',
+				'Site',
+				'User',
+			]
+
+			for m in MODELS:
+				i = 0
+				qry = getattr(models, m).query()
+				for ms in qry.iter(keys_only=True):
+					ms.delete_async()
+					i += 1
+				logging.critical('deleted %i %s entities', i, m)
+
+			i = 0
+			bd = []
+			for b in blobstore.BlobInfo.all():
+				bd.append(blobstore.delete_async(b.key()))
+				i += 1
+			for b in bd:
+				b.get_result()
+
+			logging.critical('deleted %i blobs', i)
+
+			from google.appengine.api import memcache
+			memcache.flush_all()
+			logging.critical('flushed memcache')
+
+			self.redirect(webapp2.uri_for('main'))
+
+class Admin(BaseHandler):
+	def get(self):
+		self.render('admin.html', {
+			'drafts': list(models.SiteBlogPost.drafts()),
+			'posts': models.SiteBlogPost.published(),
+			'themes': models.THEMES,
+		})
+
+class AdminNewPost(BaseHandler):
+	def get(self):
+		sbpid = models.SiteBlogPost.allocate_ids(size=1)[0]
+		sbpkey = ndb.Key('SiteBlogPost', sbpid)
+
+		im = models.SiteImage(parent=sbpkey, width=620, height=412)
+		im.put()
+
+		p = models.SiteBlogPost(
+			key=sbpkey,
+			title='Title',
+			author=users.get_current_user().nickname(),
+			image=im.key
+		)
+		p.put()
+		self.redirect(webapp2.uri_for('admin-edit-post', postid=p.key.id()))
+
+class AdminEditPost(BaseHandler):
+	DATE_FMT = '%Y-%m-%d %H:%M'
+
+	def get(self, postid):
+		p = models.SiteBlogPost.get_by_id(long(postid))
+		self.render('admin-edit-post.html', {
+			'fmt': self.DATE_FMT,
+			'i': p.image.get(),
+			'p': p,
+		})
+
+	def post(self, postid):
+		p = models.SiteBlogPost.get_by_id(long(postid))
+
+		for k in ['title', 'text', 'link']:
+			v = self.request.get(k)
+			if v:
+				if k == 'link':
+					v = models.link_filter(None, v)
+					if p.link != v and models.SiteBlogPost.link_key(v):
+						raise ValueError('link %s already in use' %v)
+
+				setattr(p, k, self.request.get(k))
+
+		p.date = datetime.datetime.strptime(self.request.get('date'), self.DATE_FMT)
+		p.tags = [t.strip() for t in self.request.get('tags').split(',') if t.strip()]
+		p.autolink = self.request.get('autolink') == 'on'
+
+		d = self.request.get('draft') == 'on'
+		a = self.request.get('author').strip()
+
+		c = models.Config.get_by_id(models.CONFIG_AUTHORS)
+		if not c:
+			c = models.Config(id=models.CONFIG_AUTHORS, data={})
+
+		if d and not p.draft:
+			c.data[a] -= 1
+		elif not d and p.draft:
+			c.data.setdefault(a, 0)
+			c.data[a] += 1
+		elif not p.draft and a != p.author:
+			if p.author in c.data:
+				c.data[p.author] -= 1
+
+			if a not in c.data:
+				c.data[a] = 0
+
+			c.data[a] += 1
+
+		p.author = a
+		p.draft = d
+
+		for k, v in c.data.items():
+			if not v:
+				del c.data[k]
+
+		ndb.put_multi([p, c])
+		self.redirect(webapp2.uri_for('admin-edit-post', postid=postid))
+
+class AdminBlogImage(BaseHandler):
+	def get(self, postid):
+		p = models.SiteBlogPost.get_by_id(long(postid))
+
+		self.render('admin-blog-image.html', {
+			'i': p.image.get(),
+			'p': p,
+			'url': blobstore.create_upload_url(webapp2.uri_for('admin-upload-image', postid=postid)),
+		})
+
+class AdminUploadImage(BaseUploadHandler):
+	@ndb.toplevel
+	def post(self, postid):
+		postid = long(postid)
+
+		if postid:
+			p = models.SiteBlogPost.get_by_id(postid)
+
+		uploads = self.get_uploads()
+
+		if uploads[0].content_type.startswith('image/') and len(uploads) == 1:
+			upload = uploads[0]
+			i = Image.open(upload.open())
+			w, h = i.size
+
+			if postid:
+				blob = models.SiteImageBlob(
+					parent=p.key,
+					blob=upload.key(),
+					size=upload.size,
+					name=upload.filename,
+					width=w,
+					height=h
+				)
+				blob.put()
+
+				im = p.image.get()
+				im.set_type(models.IMAGE_TYPE_BLOB, blob)
+				im.set_blob()
+				im.put_async()
+				self.redirect(webapp2.uri_for('admin-edit-post', postid=postid))
+			else:
+				blob = models.SiteImageBlob(
+					blob=upload.key(),
+					size=upload.size,
+					name=upload.filename,
+					width=w,
+					height=h
+				)
+				blob.put()
+				self.redirect(webapp2.uri_for('admin-images'))
+		else:
+			for upload in uploads:
+				upload.delete()
+
+class AdminNewImage(BaseHandler):
+	def get(self):
+		self.render('admin-new-image.html', {
+			'url': blobstore.create_upload_url(webapp2.uri_for('admin-upload-image')),
+		})
+
+class AdminImages(BaseHandler):
+	def get(self):
+		self.render('admin-images.html', {
+			'images': models.SiteImageBlob.images(),
+		})
+
+class AdminSyncAuthors(BaseHandler):
+	def get(self):
+		c = models.Config(id=models.CONFIG_AUTHORS, data={})
+		for p in models.SiteBlogPost.published():
+			c.data.setdefault(p.author, 0)
+			c.data[p.author] += 1
+		c.put()
+
+		self.redirect(webapp2.uri_for('admin'))
+
+class Colors(BaseHandler):
+	def get(self, theme, pagename=''):
+		if theme not in models.THEMES:
+			return
+
+		user, site = self.us()
+		pages = dict([(i.key, i) for i in ndb.get_multi(site.pages)])
+
+		if not pagename:
+			page = pages[site.pages[0]]
+		else:
+			for p in pages.values():
+				if p.name == pagename:
+					page = p
+					break
+			else:
+				page = None
+
+		images = ndb.get_multi(page.images)
+		basedir = 'themes/%s/' %site.theme
+		colors = models.Color.get(theme)
+
+		saved = models.ColorSaved.theme(theme)
+
+		self.render('colors.html', {
+			'basetemplate': basedir + 'index.html',
+			'base': '/static/' + basedir,
+			'colors': colors,
+			'get': self.request.GET,
+			'images': images,
+			'mode': 'colors',
+			'page': page,
+			'pagenum': 0,
+			'pages': pages,
+			'pagetemplate': basedir + page.type + '.html',
+			'rel': webapp2.uri_for('admin-colors', theme=theme) + '/',
+			'saved': saved,
+			'site': site,
+			'theme': theme,
+		})
+
+class ColorsLess(BaseHandler):
+	@ndb.toplevel
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		color = models.Color.get(theme)
+
+		for k, v in color.data.iteritems():
+			self.response.write('@%s: #%06x;\n' %(k, v))
+
+		f = open(os.path.join('styles', theme + '.less')).read()
+		self.response.write(f)
+
+class ColorSave(BaseHandler):
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		name = self.request.get('name')
+		color = int(self.request.get('color')[1:], 16)
+
+		def callback():
+			c = models.Color.get_by_id(theme)
+			if name in c.data:
+				c.data[name] = color
+			c.put()
+
+		ndb.transaction(callback)
+
+class ColorReset(BaseHandler):
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		color = models.Color.get(theme)
+		color.key.delete()
+		self.redirect(webapp2.uri_for('admin-colors', theme=theme))
+
+class ColorCommit(BaseHandler):
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		color = models.Color.get(theme)
+		name = self.request.get('_name')
+		if not name:
+			return
+
+		c = models.ColorSaved(id=name, parent=color.key)
+		c.data = {}
+
+		for k, v in color.data.iteritems():
+			d = self.request.get(k)
+			if not d:
+				return
+
+			c.data[k] = int(d[1:], 16)
+
+		c.put()
+
+class ColorLoad(BaseHandler):
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		p = ndb.Key('Color', theme)
+		color = models.ColorSaved.get_by_id(self.request.get('name'), parent=p)
+		if not color:
+			return
+
+		c = models.Color.get(theme)
+		c.data = color.data
+		c.put()
+
+class ColorDelete(BaseHandler):
+	def get(self, theme):
+		if theme not in models.THEMES:
+			return
+
+		p = ndb.Key('Color', theme)
+		color = models.ColorSaved.get_by_id(self.request.get('name'), parent=p)
+		if not color:
+			return
+		color.key.delete()
---
+++ edit.py
@@ -0,0 +1,593 @@
+# Copyright (c) 2012 Matt Jibson <matt.jibson@gmail.com>
+
+import datetime
+import json
+import logging
+
+from PIL import Image
+from google.appengine.ext import blobstore
+from google.appengine.ext import deferred
+from google.appengine.ext import ndb
+import webapp2
+
+from main import BaseHandler, BaseUploadHandler
+import models
+import settings
+import utils
+
+class Edit(BaseHandler):
+	def get(self, pagename=None, pagenum=0):
+		user, site = self.us()
+
+		if not user or not site:
+			self.add_message('error', 'Not logged in')
+			self.redirect(webapp2.uri_for('main'))
+			return
+
+		pages = dict([(i.key, i) for i in ndb.get_multi(site.pages)])
+		basedir = 'themes/%s/' %site.theme
+
+		page = pages[site.pages[0]]
+		if pagename:
+			for k, v in pages.items():
+				if v.name == pagename:
+					page = v
+					break
+
+		if page.type == models.PAGE_TYPE_GALLERY:
+			images = ndb.get_multi(page.images)
+		else:
+			images = ndb.get_multi(page.images[:len(page.spec().get('images', []))])
+
+		all_images = models.ImageBlob.query(ancestor=site.key).fetch()
+		pagenum = int(pagenum)
+
+		self.render('edit.html', {
+			'all_images': all_images,
+			'base': '/static/' + basedir,
+			'get': self.request.GET,
+			'images': images,
+			'mode': 'edit',
+			'page': page,
+			'pagenum': pagenum,
+			'pages': pages,
+			'pagetemplate': basedir + page.type + '.html',
+			'publish_url': webapp2.uri_for('publish', sitename=site.name),
+			'published_url': 'http://commondatastorage.googleapis.com/' + settings.BUCKET_NAME + '/' + site.key.id() + '/' + page.name,
+			'rel': webapp2.uri_for('edit-home') + '/',
+			'site': site,
+			'template': basedir + 'index.html',
+			'upload_url': webapp2.uri_for('upload-url', sitename=site.name, pageid=page.key.id()),
+			'view_url': webapp2.uri_for('view-page', sitename=site.name, pagename=page.name, pagenum=pagenum),
+		})
+
+class Save(BaseHandler):
+	@ndb.toplevel
+	def post(self, siteid, pageid):
+		skey = ndb.Key('Site', siteid)
+		pkey = ndb.Key('Page', long(pageid), parent=skey)
+		keys = [
+			'headline',
+		]
+
+		keys.extend([i[0] for i in models.Site.social_media])
+
+		logging.error(self.request.POST)
+
+		r = {'errors': []}
+
+		set_domain = False
+		if '_domain' in self.request.POST:
+			v = self.request.POST.get('_domain')
+
+			if not v:
+				domain = None
+				set_domain = True
+			else:
+				# A race condition exists here: after this check and before the transaction
+				# completes another site could switch to this domain. However, we can't run
+				# an ancestor-less query in a transaction, so just ignore it.
+				d = models.Site.domain_exists(v)
+				if not d:
+					domain = v
+					set_domain = True
+				elif d != skey:
+					r['errors'].append('The domain %s is already being used' %v)
+
+		def callback():
+			s, p = ndb.get_multi([skey, pkey])
+			sc, pc = False, False
+
+			if not s or not p or p.key.parent() != s.key:
+				return
+
+			for k in keys:
+				v = self.request.POST.get('_%s' %k, None)
+				if v is not None:
+					setattr(s, k, v)
+					sc = True
+
+			if set_domain:
+				s.domain = domain
+				sc = True
+
+			pos = self.request.POST.get('pos')
+			if pos:
+				pages = []
+				for p_pos in pos.split(','):
+					if not p_pos.startswith('p_'):
+						continue
+					pages.append(ndb.Key('Page', long(p_pos[2:]), parent=skey))
+				s.pages = pages
+				sc = True
+
+			if sc:
+				s.put_async()
+
+			spec = p.spec()
+
+			for i in range(spec.get('links', 0)):
+				k = '_link_%i' %i
+				if k in self.request.POST:
+					p.links[i] = self.request.POST[k]
+					pc = True
+
+			for i in range(spec.get('text', 0)):
+				k = '_text_%i' %i
+				if k in self.request.POST:
+					p.text[i] = self.request.POST[k]
+					pc = True
+
+			for i in range(spec.get('lines', 0)):
+				k = '_line_%i' %i
+				if k in self.request.POST:
+					p.lines[i] = self.request.POST[k]
+					pc = True
+
+			cm = self.request.POST.get('p_%s_name' %p.key.id())
+			if cm:
+				errors = models.Page.pagename_isvalid(s, cm)
+				if cm.lower() != p.name_lower and errors:
+					r['errors'].append(errors)
+				else:
+					p.name = cm
+					pc = True
+			elif 'current_menu' in self.request.POST:
+				r['errors'].append('Page names may not be blank')
+
+			if 'gal' in self.request.POST and p.type == models.PAGE_TYPE_GALLERY:
+				p.images = []
+				for i in self.request.POST.get('gal').split(','):
+					if not i:
+						continue
+					imgid = long(i.partition('_')[2])
+					p.images.append(ndb.Key('ImageBlob', imgid, parent=skey))
+				pc = True
+
+			if p.type == models.PAGE_TYPE_BLOG:
+				# what if we have multiple puts on the same entity here? race condition?
+				for k in self.request.POST.keys():
+					value = None
+
+					if k.startswith('_posttitle_'):
+						name = 'title'
+					elif k.startswith('_posttext_'):
+						name = 'text'
+					elif k.startswith('_postauthor_'):
+						name = 'author'
+					elif k.startswith('_postdate_'):
+						name = 'date'
+						d = self.request.POST[k].split('-')
+						value = datetime.datetime(int(d[0]), int(d[1]) + 1, int(d[2]))
+					elif k.startswith('_postdraft_'):
+						name = 'draft'
+						value = self.request.POST[k] == 'true'
+					else:
+						continue
+
+					bpid = long(k.rpartition('_')[2])
+					bp = models.BlogPost.get_by_id(bpid, parent=p.key)
+
+					if value is None:
+						value = self.request.POST[k]
+
+					setattr(bp, name, value)
+					bp.put_async()
+
+			if pc:
+				p.put_async()
+
+			return [s, p]
+
+		s, p = ndb.transaction(callback)
+		spec = p.spec()
+
+		def proc_img(k, imkey):
+			k += '_'
+			kx, ky, ks, kc, kb = k + 'x', k + 'y', k + 's', k + 'c', k + 'b'
+
+			if kx in self.request.POST and ky in self.request.POST and ks in self.request.POST:
+				img = imkey.get()
+				img.x = int(self.request.POST[kx].partition('.')[0])
+				img.y = int(self.request.POST[ky].partition('.')[0])
+				img.s = float(self.request.POST[ks])
+				img.set_blob()
+				img.put_async()
+				return {'url': img.url}
+			elif kc in self.request.POST:
+				img = imkey.get()
+				img.set_type(models.IMAGE_TYPE_HOLDER)
+				img.put_async()
+				return {'url': img.url}
+			elif kb in self.request.POST:
+				blob = ndb.Key('ImageBlob', long(self.request.POST[kb]), parent=s.key).get()
+				if blob:
+					img = imkey.get()
+					img.set_type(models.IMAGE_TYPE_BLOB, blob)
+					img.set_blob()
+					img.put_async()
+					return {
+						'baseh': img.oh,
+						'basew': img.ow,
+						'orig': img.orig,
+						'url': img.url,
+					}
+
+		for i in range(len(spec.get('images', []))):
+			k = '_image_%i' %i
+			ret = proc_img(k, p.images[i])
+			if ret:
+				r[k] = ret
+
+		proc_imgs = set()
+		for k in self.request.POST.keys():
+			if k.startswith('_postimage_'):
+				proc_imgs.add(long(k.split('_')[2]))
+
+		for img in proc_imgs:
+			k = '_postimage_%i' %img
+			ret = proc_img(k, p.get_blogpost(img).image)
+			if ret:
+				r[k] = ret
+
+		self.response.out.write(json.dumps(dict(r)))
+
+class GetUploadURL(BaseHandler):
+	def get(self, sitename, pageid):
+		skey = ndb.Key('Site', sitename)
+		pkey = ndb.Key('Page', long(pageid), parent=skey)
+		site, page = ndb.get_multi([skey, pkey])
+
+		r = self.request.get('image').rpartition('_')
+		ikey = r[0]
+		image = int(r[2])
+
+		if site and page and site.user.urlsafe() == self.session['user']['key'] and (image <= len(page.images) or ikey == '_postimage'):
+			self.response.out.write(blobstore.create_upload_url(
+				webapp2.uri_for('upload-file',
+					sitename=sitename,
+					pageid=pageid,
+					image=str(image)
+				)
+			))
+			logging.error('YES')
+		else:
+			logging.error(r)
+			self.response.out.write('')
+
+class UploadHandler(BaseUploadHandler):
+	@ndb.toplevel
+	def post(self, sitename, pageid, image):
+		skey = ndb.Key('Site', sitename)
+		pkey = ndb.Key('Page', long(pageid), parent=skey)
+		site, page = ndb.get_multi([skey, pkey])
+		image = int(image)
+		uploads = self.get_uploads()
+
+		# todo: return useful error code if content type is not image/*
+		if site and site.user.urlsafe() == self.session['user']['key'] and image <= len(page.images) and len(uploads) == 1 and uploads[0].content_type.startswith('image/'):
+			upload = uploads[0]
+			i = Image.open(upload.open())
+			w, h = i.size
+			blob = models.ImageBlob(
+				parent=site.key,
+				blob=upload.key(),
+				size=upload.size,
+				name=upload.filename,
+				width=w,
+				height=h
+			)
+			blob.put()
+
+			def callback():
+				s = skey.get()
+				s.size += blob.size
+				s.put()
+				return s
+
+			i_f = page.images[image].get_async()
+
+			s = ndb.transaction(callback)
+
+			i = i_f.get_result()
+			i.set_type(models.IMAGE_TYPE_BLOB, blob)
+			i.set_blob()
+			i.put_async()
+			self.redirect(webapp2.uri_for('upload-success',
+				url=i.url,
+				orig=i.orig,
+				w=i.ow,
+				h=i.oh,
+				s=i.s,
+				name=blob.name,
+				id=blob.key.id()
+			))
+		else:
+			for upload in uploads:
+				upload.delete()
+
+class UploadSuccess(BaseHandler):
+	def get(self):
+		self.response.out.write(json.dumps(dict(self.request.GET)))
+
+class View(BaseHandler):
+	def get(self, sitename, pagename=None, pagenum=0):
+		site = ndb.Key('Site', sitename).get()
+		pages = dict([(i.key, i) for i in ndb.get_multi(site.pages)])
+
+		if not pagename:
+			page = pages[site.pages[0]]
+		else:
+			for p in pages.values():
+				if p.name == pagename:
+					page = p
+					break
+			else:
+				page = None
+
+		if not site or not page or site.user.urlsafe() != self.session['user']['key']:
+			return
+
+		images = ndb.get_multi(page.images)
+		basedir = 'themes/%s/' %site.theme
+
+		self.render(basedir + 'index.html', {
+			'base': '/static/' + basedir,
+			'get': self.request.GET,
+			'images': images,
+			'mode': 'view',
+			'page': page,
+			'pagenum': int(pagenum),
+			'pages': pages,
+			'pagetemplate': basedir + page.type + '.html',
+			'rel': webapp2.uri_for('view-home', sitename=sitename) + '/',
+			'site': site,
+		})
+
+class Reset(BaseHandler):
+	@ndb.toplevel
+	def get(self):
+		user, site = self.us()
+
+		if not user or not site:
+			self.add_message('error', 'Not logged in')
+			self.redirect(webapp2.uri_for('main'))
+			return
+
+		pages = ndb.get_multi(site.pages)
+		ndb.delete_multi_async(site.pages)
+		for n, p in enumerate(pages):
+			pages[n] = models.Page.new(p.name, site, p.type)
+		site.pages = [i.key for i in pages]
+		ndb.put_multi(pages)
+		site.put()
+		self.redirect(webapp2.uri_for('edit-home'))
+
+class Publish(BaseHandler):
+	def get(self, sitename):
+		site = ndb.Key('Site', sitename).get()
+
+		if not site or site.user.urlsafe() != self.session['user']['key'] or site.do_publish:
+			return
+
+		def callback():
+			s = site.key.get()
+			if not s.do_publish:
+				s.do_publish = True
+				deferred.defer(publish_site, sitename)
+				s.put()
+
+		ndb.transaction(callback)
+
+class Layout(BaseHandler):
+	def get(self, siteid, pageid, layoutid):
+		user, site = self.us()
+
+		if site.key.id() != siteid:
+			return
+
+		page = ndb.Key('Page', long(pageid), parent=site.key).get()
+		if not page:
+			return
+
+		page = models.Page.set_layout(page, long(layoutid))
+		self.redirect(webapp2.uri_for('edit', pagename=page.name))
+
+class SetColors(BaseHandler):
+	def get(self, siteid, color):
+		user, site = self.us()
+
+		if site.key.id() != siteid:
+			return
+
+		if color in models.COLORS[site.theme]:
+
+			def callback():
+				s = site.key.get()
+				s.color = color
+				s.put()
+
+			ndb.transaction(callback)
+
+		self.redirect(webapp2.uri_for('edit-home'))
+
+class NewPage(BaseHandler):
+	def post(self, pagetype, layoutid):
+		user, site = self.us()
+
+		title = self.request.get('title').strip()
+		if not title:
+			title = pagetype
+
+		error = models.Page.pagename_isvalid(site, title)
+		if error:
+			self.response.out.write(json.dumps({
+				'error': error,
+			}))
+			return
+
+		layout = int(layoutid)
+		page = models.Page.new(title, site, pagetype, layout)
+
+		def callback():
+			s = site.key.get()
+			s.pages.append(page.key)
+			s.put()
+			return s
+
+		s = ndb.transaction(callback)
+		self.response.out.write(json.dumps({
+			'success': webapp2.uri_for('edit', pagename=page.name),
+		}))
+
+class NewBlogPost(BaseHandler):
+	@ndb.toplevel
+	def get(self, pageid):
+		user, site = self.us()
+		page = ndb.Key('Page', long(pageid), parent=site.key).get()
+
+		if not user or not page:
+			return
+
+		spec = page.spec()
+
+		bpid = models.BlogPost.allocate_ids(size=1, parent=page.key)[0]
+		bpkey = ndb.Key('BlogPost', bpid, parent=page.key)
+
+		imkey = ndb.Key('Image', '0', parent=bpkey)
+		im = models.Image(key=imkey, width=spec['postimagesz'][0], height=spec['postimagesz'][1])
+
+		bp = models.BlogPost(
+			key=bpkey,
+			title='Post Title',
+			author=user.first_name,
+			image=imkey
+		)
+
+		im.put_async()
+		bp.put_async()
+		self.redirect(webapp2.uri_for('edit-page', pagename=page.name, pagenum=bpid))
+
+class ArchivePage(BaseHandler):
+	def get(self, pageid):
+		user, site = self.us()
+		self.redirect(webapp2.uri_for('edit-home'))
+
+		if not user or not site:
+			return
+
+		pkey = ndb.Key('Page', long(pageid), parent=site.key)
+		if pkey in site.pages:
+			def callback():
+				s = site.key.get()
+				s.pages.remove(pkey)
+				s.put()
+				return s
+
+			s = ndb.transaction(callback)
+
+class UnarchivePage(BaseHandler):
+	def post(self):
+		user, site = self.us()
+		self.redirect(webapp2.uri_for('edit-home'))
+
+		if not user or not site or 'pageid' not in self.request.POST:
+			return
+
+		pkey = ndb.Key('Page', long(self.request.get('pageid')), parent=site.key)
+		p = pkey.get()
+		if p:
+			def callback():
+				s = site.key.get()
+				s.pages.append(pkey)
+				s.put()
+				return s
+
+			ndb.transaction(callback)
+			self.redirect(webapp2.uri_for('edit', pagename=p.name))
+
+def publish_site(sitename):
+	site = ndb.Key('Site', sitename).get()
+	pages = dict([(i.key, i) for i in ndb.get_multi(site.pages)])
+
+	if not site or not pages or not site.do_publish:
+		return
+
+	def callback():
+		s = site.key.get()
+		if not s.do_publish:
+			return None
+
+		s.do_publish = False
+		s.last_published = datetime.datetime.now()
+		s.last_published_num += 1
+		s.put()
+		return s
+
+	site = ndb.transaction(callback)
+	if not site:
+		return
+
+	basedir = 'themes/%s/' %site.theme
+	rel = settings.BUCKET_NAME + '/' + site.key.id() + '/'
+
+	def write_page(pagenum=0):
+		c = utils.render(basedir + 'index.html', {
+			'base': settings.TNM_URL + '/static/' + basedir,
+			'images': images,
+			'mode': 'publish',
+			'page': page,
+			'pagenum': pagenum,
+			'pages': pages,
+			'pagetemplate': basedir + page.type + '.html',
+			'rel': '/' + rel,
+			'site': site,
+		})
+
+		name = '/%i' %pagenum if pagenum else ''
+		page.gs_write(name, 'text/html', c)
+
+	for page in pages.values():
+		if page.type not in [
+				models.PAGE_TYPE_GALLERY,
+				models.PAGE_TYPE_HOME,
+				models.PAGE_TYPE_TEXT,
+			]:
+			continue
+
+		images = ndb.get_multi(page.images)
+
+		write_page()
+
+		# Some pages need to support multiple pages, must hard code all such pages
+		# and generate them here.
+
+		if site.theme == models.THEME_MARCO and page.type == models.PAGE_TYPE_GALLERY and page.layout == 2:
+			rows = page.spec()['rows']
+			rowsz = page.spec()['rows']
+			pgs = len(images) / (rows * rowsz) + 1
+			for i in range(1, pgs + 1):
+				write_page(i)
+
+	p = models.Publish(id=site.last_published_num, parent=site.key)
+	p.put()
