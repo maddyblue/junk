6b1932bf690e73800093f06f304cd8eaad05fbd3
Matt Jibson <matt.jibson@gmail.com>
2012-11-03 02:27:56 -0400

    Add working filesizeformat filter

--- filters.py
+++ filters.py
@@ -96,6 +96,36 @@ def tweets(handle, el):
 def layoutimg(theme, pagetype, layout):
 	return "/static/images/layouts/%s/%s-%i.png" %(theme, pagetype, layout)
 
+# filesizeformat in jinja 2.6 is broken, use this from their current github
+def filesizeformat(value, binary=False):
+	"""Format the value like a 'human-readable' file size (i.e. 13 kB,
+	4.1 MB, 102 Bytes, etc).  Per default decimal prefixes are used (Mega,
+	Giga, etc.), if the second parameter is set to `True` the binary
+	prefixes are used (Mebi, Gibi).
+	"""
+	bytes = float(value)
+	base = binary and 1024 or 1000
+	prefixes = [
+		(binary and 'KiB' or 'kB'),
+		(binary and 'MiB' or 'MB'),
+		(binary and 'GiB' or 'GB'),
+		(binary and 'TiB' or 'TB'),
+		(binary and 'PiB' or 'PB'),
+		(binary and 'EiB' or 'EB'),
+		(binary and 'ZiB' or 'ZB'),
+		(binary and 'YiB' or 'YB')
+	]
+	if bytes == 1:
+		return '1 Byte'
+	elif bytes < base:
+		return '%d Bytes' % bytes
+	else:
+		for i, prefix in enumerate(prefixes):
+			unit = base ** (i + 2)
+			if bytes < unit:
+				return '%.1f %s' % ((base * bytes / unit), prefix)
+		return '%.1f %s' % ((base * bytes / unit), prefix)
+
 filters = dict([(i, globals()[i]) for i in [
 	'date',
 	'editline',
@@ -107,6 +137,7 @@ filters = dict([(i, globals()[i]) for i in [
 	'editposttitle',
 	'edittext',
 	'fdate',
+	'filesizeformat',
 	'layoutimg',
 	'link',
 	'linkmap',
