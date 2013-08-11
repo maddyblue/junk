58129749c7a91cd55b1b25c0de25303458b1675d
Matt Jibson <matt.jibson@gmail.com>
2013-08-10 23:55:52 -0400

    AccountPicker

--- goread/src/main/java/com/goread/reader/MainActivity.java
+++ goread/src/main/java/com/goread/reader/MainActivity.java
@@ -1,15 +1,24 @@
 package com.goread.reader;
 
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+
 public class MainActivity extends Activity {
+
+    static final int PICK_ACCOUNT_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE}, false, null, null, null, null);
+        startActivityForResult(intent, PICK_ACCOUNT_REQUEST);
     }
 
 
@@ -19,5 +28,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == PICK_ACCOUNT_REQUEST && resultCode == RESULT_OK)
+        {
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+        }
+    }
 }
