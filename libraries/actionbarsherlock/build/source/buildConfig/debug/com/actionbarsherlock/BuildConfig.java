287851090659ac5b6ee6afc7be8e8ec43008b182
Matt Jibson <matt.jibson@gmail.com>
2013-10-06 06:47:03 -0400

    Sync

--- libraries/actionbarsherlock/build/source/buildConfig/debug/com/actionbarsherlock/BuildConfig.java
+++ libraries/actionbarsherlock/build/source/buildConfig/debug/com/actionbarsherlock/BuildConfig.java
@@ -2,6 +2,6 @@
 package com.actionbarsherlock;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
 
 }
--- libraries/actionbarsherlock/build/source/buildConfig/release/com/actionbarsherlock/BuildConfig.java
+++ libraries/actionbarsherlock/build/source/buildConfig/release/com/actionbarsherlock/BuildConfig.java
@@ -2,6 +2,6 @@
 package com.actionbarsherlock;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = false;
+    public static final boolean DEBUG = false;
 
 }
