30b16c991226199ce1d5ce93dede93ac8c1dd874
Matt Jibson <matt.jibson@gmail.com>
2014-04-03 00:26:13 -0400

    File copying

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,11 +18,29 @@ import (
 	"code.google.com/p/go.tools/astutil"
 )
 
-var dryrun = flag.Bool("n", false, "don't perform any action, instead print them")
-var relpath string
+const ThirdParty = "third_party"
+const dirMode os.FileMode = 0755
+
+var (
+	dryrun  = flag.Bool("n", false, "don't perform any action, instead print them")
+	create  = flag.Bool("c", false, "create the "+ThirdParty+" directory if needed")
+	verbose = flag.Bool("v", false, "print actions")
+
+	relpath, gopath string
+)
 
 func main() {
 	flag.Parse()
+	if *dryrun {
+		*verbose = true
+	}
+
+	if *create {
+		os.Mkdir(ThirdParty, dirMode)
+	}
+	if _, err := os.Open(ThirdParty); err != nil {
+		log.Fatalf("could not open %s directory: run with -c or create it manually", ThirdParty)
+	}
 
 	pwd, err := os.Getwd()
 	if err != nil {
@@ -35,13 +54,19 @@ func main() {
 		if !strings.HasPrefix(pwd, s) {
 			continue
 		}
+		gopath = s
 		relpath = strings.TrimPrefix(pwd, filepath.Join(s, "src")+string(os.PathSeparator))
 	}
 	if relpath == "" {
 		log.Fatal("couldn't determine current directory relative to $GOPATH")
 	}
 
-	rewriteImports()
+	for {
+		update()
+		if !rewriteImports() {
+			break
+		}
+	}
 }
 
 var ImportSites = []string{
@@ -52,8 +77,96 @@ var ImportSites = []string{
 	"launchpad.net",
 }
 
-func rewriteImports() {
+func update() {
+	paths := make(map[string]struct{})
+	reltp := filepath.ToSlash(relpath) + "/" + ThirdParty + "/"
 	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return nil
+		}
+		fset := token.NewFileSet()
+		f, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+		if err != nil {
+			return nil
+		}
+		for _, im := range f.Imports {
+			v := im.Path.Value[1 : len(im.Path.Value)-1]
+			if !strings.HasPrefix(v, reltp) {
+				continue
+			}
+			paths[strings.TrimPrefix(v, reltp)] = struct{}{}
+		}
+		return nil
+	})
+	for k := range paths {
+		fpath := filepath.Join(gopath, "src", k)
+		f, err := os.Open(fpath)
+		if err != nil {
+			log.Printf("%s required, but could not be found at %s", k, fpath)
+			continue
+		}
+		files, err := f.Readdir(0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Close()
+		for _, source := range files {
+			if source.IsDir() || strings.HasPrefix(source.Name(), ".") {
+				continue
+			}
+			spath := filepath.Join(f.Name(), source.Name())
+			destdir := filepath.Join(ThirdParty, k)
+			dest := filepath.Join(destdir, source.Name())
+			if err := os.MkdirAll(destdir, dirMode); err != nil {
+				log.Fatal(err)
+			}
+			dst, err := os.Open(dest)
+			created := false
+			newer := false
+			if os.IsNotExist(err) {
+				if *verbose {
+					log.Println("create", dest)
+				}
+				created = true
+			} else if err != nil {
+				log.Fatal(err)
+			} else {
+				destination, err := dst.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				newer = source.ModTime().After(destination.ModTime())
+				dst.Close()
+			}
+			if created || newer {
+				if *verbose {
+					log.Printf("copy %s -> %s\n", spath, dest)
+				}
+				if !*dryrun {
+					src, err := os.Open(spath)
+					if err != nil {
+						log.Fatal(err)
+					}
+					dst, err = os.OpenFile(dest, os.O_RDWR|os.O_CREATE|os.O_TRUNC, source.Mode())
+					if err != nil {
+						log.Fatal(err)
+					}
+					if _, err := io.Copy(dst, src); err != nil {
+						log.Fatal(err)
+					}
+					src.Close()
+					dst.Close()
+				}
+			}
+		}
+	}
+}
+
+func rewriteImports() (rewritten bool) {
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return nil
+		}
 		fset := token.NewFileSet()
 		f, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
 		if err != nil {
@@ -67,14 +180,17 @@ func rewriteImports() {
 					continue
 				}
 				if strings.HasPrefix(v, site+"/") {
-					target := fmt.Sprintf("%s/third_party/%s", relpath, v)
+					target := fmt.Sprintf("%s/%s/%s", relpath, ThirdParty, v)
+					if *verbose {
+						log.Printf("rewrite %s: %s -> %s\n", path, v, target)
+					}
 					if *dryrun {
-						fmt.Printf("%s: %s -> %s\n", path, v, target)
 						break
 					}
 					if !astutil.RewriteImport(fset, f, v, target) {
 						log.Fatalf("%s: could not rewrite %s", path, im.Path.Value)
 					}
+					rewritten = true
 					break
 				}
 			}
@@ -92,4 +208,5 @@ func rewriteImports() {
 		}
 		return nil
 	})
+	return
 }
