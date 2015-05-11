b9b93c7f0f7b91c3766b9a1cf49af58ea6f3e6dd
Matt Jibson <matt.jibson@gmail.com>
2015-05-11 03:13:27 -0400

    Don't depend on pages for packets

--- vorbis.go
+++ vorbis.go
@@ -10,7 +10,9 @@ import (
 )
 
 type Vorbis struct {
-	o *ogg.Decoder
+	o   *ogg.Decoder
+	br  *bitReader
+	Err error
 
 	Version     uint32
 	Channels    uint8
@@ -37,53 +39,64 @@ func NewVorbis(r io.Reader) (*Vorbis, error) {
 		if err := step(); err != nil {
 			return nil, err
 		}
+		if v.Err != nil {
+			return nil, v.Err
+		}
 	}
 	for i := 0; true; i++ {
-		_, err := v.decode(0)
-		if err != nil {
+		if err := v.decode(0); err != nil {
 			break
 		}
 	}
 	return v, nil
 }
 
-type oggPage struct {
-	ogg.Page
-	*bitReader
+func (v *Vorbis) ReadBits(bits uint) uint32 {
+	if v.br == nil {
+		p, err := v.o.Decode()
+		if err != nil {
+			v.Err = err
+			return 0
+		}
+		br := newBitReader(bytes.NewReader(p.Packet))
+		v.br = &br
+	}
+	b := v.br.ReadBits(bits)
+	err := v.br.Err()
+	if err != nil {
+		v.br = nil
+		return v.ReadBits(bits)
+	}
+	return b
 }
 
-func (o *oggPage) Read(bits int) uint32 {
+func (v *Vorbis) Read(bits int) uint32 {
 	var u uint32
 	for offset := uint(0); bits > 0; bits, offset = bits-8, offset+8 {
 		rem := uint(bits)
 		if rem > 8 {
 			rem = 8
 		}
-		v := o.ReadBits(rem)
+		v := v.ReadBits(rem)
 		u |= v << offset
 	}
 	return u
 }
 
-func (v *Vorbis) decode(typ uint8) (*oggPage, error) {
-	p, err := v.o.Decode()
-	if err != nil {
-		return nil, err
-	}
-	br := newBitReader(bytes.NewReader(p.Packet))
-	o := &oggPage{
-		Page:      p,
-		bitReader: &br,
-	}
-	if t := uint8(o.ReadBits(8)); t != typ {
-		return nil, fmt.Errorf("unexpected header %02x, expected %02x", t, typ)
+func (v *Vorbis) ReadByte() byte {
+	return byte(v.ReadBits(8))
+}
+
+func (v *Vorbis) decode(typ uint8) error {
+	if t := v.ReadByte(); t != typ {
+		return fmt.Errorf("unexpected header %02x, expected %02x", t, typ)
 	}
 	for _, c := range "vorbis" {
-		if b := rune(o.ReadBits(8)); b != c {
-			return nil, fmt.Errorf("unexpected character %c, expected %c", b, c)
+		if b := rune(v.ReadByte()); b != c {
+			return fmt.Errorf("unexpected character %c, expected %c", b, c)
 		}
 	}
-	return o, o.Err()
+	return nil
 }
 
 const (
@@ -101,43 +114,41 @@ var (
 )
 
 func (v *Vorbis) decodeIdentification() error {
-	p, err := v.decode(typeIdentification)
-	if err != nil {
+	if err := v.decode(typeIdentification); err != nil {
 		return err
 	}
-	v.Version = uint32(p.Read(32))
-	v.Channels = uint8(p.Read(8))
-	v.SampleRate = uint32(p.Read(32))
-	v.BitrateMax = uint32(p.Read(32))
-	v.BitrateNorm = uint32(p.Read(32))
-	v.BitrateMin = uint32(p.Read(32))
-	v.Blocksize0 = 1 << p.Read(4)
-	v.Blocksize1 = 1 << p.Read(4)
+	v.Version = uint32(v.Read(32))
+	v.Channels = v.ReadByte()
+	v.SampleRate = uint32(v.Read(32))
+	v.BitrateMax = uint32(v.Read(32))
+	v.BitrateNorm = uint32(v.Read(32))
+	v.BitrateMin = uint32(v.Read(32))
+	v.Blocksize0 = 1 << v.Read(4)
+	v.Blocksize1 = 1 << v.Read(4)
 	if v.Blocksize0 > v.Blocksize1 || v.Blocksize0 == 0 || v.Blocksize1 == 0 {
 		return fmt.Errorf("vorbis: bad blocksize")
 	}
-	if p.Read(1) != 1 {
+	if v.ReadByte() != 1 {
 		return ErrFraming
 	}
-	return p.Err()
+	return nil
 }
 
 func (v *Vorbis) decodeComment() error {
-	p, err := v.decode(typeComment)
-	if err != nil {
+	if err := v.decode(typeComment); err != nil {
 		return err
 	}
 	read := func() string {
-		l := int(p.Read(32))
+		l := int(v.Read(32))
 		bytes := make([]byte, l)
 		for i := 0; i < l; i++ {
-			bytes[i] = byte(p.Read(8))
+			bytes[i] = byte(v.ReadByte())
 		}
 		return string(bytes)
 	}
 	v.Vendor = read()
 	v.Comments = make(map[string][]string)
-	comments := int(p.Read(32))
+	comments := int(v.Read(32))
 	for i := 0; i < comments; i++ {
 		c := read()
 		sp := strings.SplitN(c, "=", 2)
@@ -146,8 +157,8 @@ func (v *Vorbis) decodeComment() error {
 		}
 		v.Comments[sp[0]] = append(v.Comments[sp[0]], sp[1])
 	}
-	if p.Read(1) != 1 {
+	if v.ReadByte() != 1 {
 		return ErrFraming
 	}
-	return p.Err()
+	return nil
 }
